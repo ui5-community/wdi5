640a0633d6f4db88101467dc84faf801bacee711
Volker Buzek <volker.buzek@js-soft.com>
2020-05-07 21:01:35 +0200

    chore(ios): emulator build

--- test/ui5-app/app/build-ios.js
+++ test/ui5-app/app/build-ios.js
@@ -4,23 +4,21 @@ const fs = require('fs-extra');
 const path = require('path');
 const {execSync} = require('child_process');
 
-console.log(`[build:ios] Preparing installation ...`);
+console.log(`[ios] Preparing installation ...`);
 
 const iosPlatformFolderPath = path.resolve('platforms', 'ios');
 
 if (fs.existsSync(iosPlatformFolderPath)) {
-    console.log(`\t> Cleaning ...`);
+    console.log(`[ios] Cleaning ...`);
     fs.removeSync(path.resolve(iosPlatformFolderPath, 'build'));
     execSync(`cordova clean ios`, {stdio: 'inherit'});
 } else {
-    console.log(`\t> Installing ios platform ...`);
+    console.log(`[ios] Installing ios platform ...`);
     execSync(`cordova platform add ios@latest`, {stdio: 'inherit'});
 }
 
-console.log(`[build:ios] Starting develop build ...`);
-// execSync(`cordova build ios --no-telemetry --debug`, {
-//     stdio: 'inherit'
-// });
-execSync(`cordova run ios --no-telemetry`, {
+console.log(`[ios] Starting build ...`);
+execSync(`cordova build ios --no-telemetry --emulator`, {
     stdio: 'inherit'
 });
+
