cab3a9b373f1e9f0222374419a5f5a2eb62b1e8e
Volker Buzek <volker.buzek@js-soft.com>
2020-05-11 15:11:02 +0200

    chore(prettier): src formatting

--- test/wdio-ios.conf.js
+++ test/wdio-ios.conf.js
@@ -1,21 +1,21 @@
-const path = require("path")
-let config = require("./wdio-shared.conf").config
+const path = require('path');
+let config = require('./wdio-shared.conf').config;
 
 config.capabilities = [
     {
-        automationName: "XCUITest",
+        automationName: 'XCUITest',
 
-        platformName: "iOS",
+        platformName: 'iOS',
 
         // The version of the Android or iOS system
-        platformVersion: "13.4",
+        platformVersion: '13.4',
 
         // For iOS, this must exactly match the device name as seen in Xcode.
-        deviceName: "iPhone SE (2nd generation)", 
+        deviceName: 'iPhone SE (2nd generation)',
 
         // Where to find the .apk or .ipa file to install on the device. The exact location
         // of the file may change depending on your Cordova version.
-        app: path.join("test", "ui5-app", "app", "platforms", "ios", "build", "emulator", "UI5.app"),
+        app: path.join('test', 'ui5-app', 'app', 'platforms', 'ios', 'build', 'emulator', 'UI5.app'),
 
         // By default, Appium runs tests in the native context. By setting autoWebview to
         // true, it runs our tests in the Cordova context.
@@ -25,14 +25,14 @@ config.capabilities = [
         // permissions automatically.
         autoGrantPermissions: true,
 
-        // display sim window - seems to default to true 
+        // display sim window - seems to default to true
         // w/ macOS 10.15.4, iOS sim 11.4.1 (921.9), appium 1.17.1
         isHeadless: false
     }
-]
+];
 
-config.outputDir = path.join("test", "report", "logs")
+config.outputDir = path.join('test', 'report', 'logs');
 
-config.wdi5.platform = "ios"
+config.wdi5.platform = 'ios';
 
-exports.config = config
+exports.config = config;
