da9ac703ec872a421b20d18219e8a6d0c931af14
dominik feininger <dominik.feininger@gmail.com>
2020-05-19 09:54:12 +0200

    feat(navigation): deep entry, hash nav
    - allow jumping into app by deep link
    - enable hash-based (#/my/path) navigation programmatically

--- test/ui5-app/webapp/test/e2e/test-ui5-ad.js
+++ test/ui5-app/webapp/test/e2e/test-ui5-ad.js
@@ -4,41 +4,38 @@ const wdi5 = require('../../../../../index');
 describe('wdio-ui5 bridge: advanced tests', () => {
     globalThis.viewName = 'test.Sample.view.Main';
 
-    it('should have the right button text', () => {
+    it('go to otherview with by hash navigation', () => {
         // as example to make sure if no other wdi5 functions are used that the SAPUI5 application is fully up and running
         wdi5().getWDioUi5().waitForUI5();
 
-        // make DOM
-        // TODO: this needs to go like this if we do get the button direktly it wont work
-        const buttonAsDom = $(
-            '/html/body/div[2]/div/div/div/div/div[3]/div/div/div[2]/div/section/div/div[3]/button/span/span/bdi'
-        ).$('../..');
-        // const buttonAsDom = $('/html/body/div[2]/div/div/div/div/div[3]/div/div/div[2]/div/section/div/div[2]/button/span/span[2]/bdi').$('..').$('..'); // to Other view
-        const expButtonText = 'IA Sync'; // IA Sync // to Other view
+        wdi5().getUtils().goTo('#/other');
 
-        if (buttonAsDom.error) {
-            // button was not found by wdio
-            const error = 'buttonAsDom.error: ' + JSON.stringify(buttonAsDom.error);
-            wdi5().getLogger().error(error);
-            throw new Error('buttonAsDom.error: ' + error);
-        }
-        // get selector as HTML as the class sap.ui.test.RecordReplay want to receive an Element
-        const buttonSelector = {
-            wdio_ui5_key: 'btmIASync',
-            selector: browser.getSelectorForElement({domElement: buttonAsDom, settings: {preferViewId: true}})
-            // alternative -> create selector yourself
-            /* selector: {
-                properties: {
-                    text: "IA Sync"
-                },
-                viewName: globalThis.viewName,
-                controlType: "sap.m.Button"
-            } */
+        // check for the new page
+        const listSelector = {
+            wdio_ui5_key: 'PeopleList',
+            selector: {
+                id: 'PeopleList',
+                viewName: 'test.Sample.view.Other'
+            }
+        };
+        assert.equal(browser.asControl(listSelector).getProperty('headerText'), '...bites the dust!');
+    });
+
+    it('go back to mainview with by hash navigation', () => {
+        wdi5().getUtils().goTo('#/main');
+        // expect to be on main page -> otherwise the next test fails ...
+
+        const cbSelector1 = {
+            wdio_ui5_key: 'cbSelector1',
+            selector: {
+                id: 'idCheckbox',
+                viewName: 'test.Sample.view.Main',
+                controlType: 'sap.m.CheckBox'
+            }
         };
 
-        // compare text in DOM
-        const ui5Button = browser.asControl(buttonSelector);
-        assert.strictEqual(ui5Button.getProperty('text'), expButtonText);
+        // initially checkbox is not selected
+        assert.ok(!browser.asControl(cbSelector1).getProperty('selected'));
     });
 
     it('check the binding of the username input', () => {
