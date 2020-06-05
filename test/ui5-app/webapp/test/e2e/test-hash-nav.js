6a6f3c4ffa4e88c2acd4789e26ecb87a90d035ac
dominik.feininger <dominik.feininger@js-soft.com>
2020-06-05 16:32:31 +0200

    fix(comments): remove

--- test/ui5-app/webapp/test/e2e/test-hash-nav.js
+++ test/ui5-app/webapp/test/e2e/test-hash-nav.js
@@ -3,9 +3,7 @@ const wdi5 = require('../../../../../index');
 
 describe('hash-based nav', () => {
     it('should allow the deep entry to "Other" view via #/Other', () => {
-        // wdi5().getUtils().takeScreenshot('hash-based nav 1.1');
         wdi5().getUtils().goTo('#/Other');
-        // wdi5().getWDioUi5().waitForUI5();
 
         const listSelector = {
             wdio_ui5_key: 'PeopleList',
@@ -17,13 +15,10 @@ describe('hash-based nav', () => {
 
         const items = browser.asControl(listSelector).getAggregation('items');
         assert.ok(items.length === 9);
-        // wdi5().getUtils().takeScreenshot('hash-based nav 1.2');
     });
 
     it('should navigate to Main view via #/', () => {
-        // wdi5().getUtils().takeScreenshot('hash-based nav 2.1');
         wdi5().getUtils().goTo('#/');
-        // wdi5().getWDioUi5().waitForUI5();
 
         const buttonSelector = {
             wdio_ui5_key: 'NavFwdButton',
@@ -34,6 +29,5 @@ describe('hash-based nav', () => {
         };
 
         assert.ok(browser.asControl(buttonSelector).isVisible())
-        // wdi5().getUtils().takeScreenshot('hash-based nav 2.2');
     })
 });
