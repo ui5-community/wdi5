ddc9819f824062d04d10d331c30d3d68cbcbb732
dominik.feininger <dominik.feininger@js-soft.com>
2020-07-21 17:25:06 +0200

    feat(methodbridge): first version
    generate functions by string

--- test/ui5-app/webapp/test/e2e/test-ui5.js
+++ test/ui5-app/webapp/test/e2e/test-ui5.js
@@ -24,18 +24,18 @@ describe('ui5 basics: properties and navigation', () => {
         wdi5().getUtils().takeScreenshot('test-ui5');
     });
 
-    it('navigation button w/ text exists', () => {
+    it.skip('navigation button w/ text exists', () => {
         assert.strictEqual(browser.asControl(buttonSelector).getProperty('text'), 'to Other view');
     });
 
-    it('getProperty("text") and getText() are equivalent', () => {
+    it.skip('getProperty("text") and getText() are equivalent', () => {
         assert.strictEqual(
             browser.asControl(buttonSelector).getProperty('text'),
             browser.asControl(buttonSelector).getText()
         );
     });
 
-    it('sets the property of a control successfully', () => {
+    it.skip('sets the property of a control successfully', () => {
         const oButton = browser.asControl(buttonSelector);
         oButton.setProperty('text', 'new button text');
 
@@ -43,7 +43,12 @@ describe('ui5 basics: properties and navigation', () => {
     });
 
     it('should navigate via button click to list page', () => {
-        browser.asControl(buttonSelector).press();
+        const button = browser.asControl(buttonSelector)
+
+        // this is the important quick test for the generated method bridge
+        button.setEnabled(false);
+        // this press should not work -> but works because it is fired by CODE -> code rules ;)
+        button.press();
 
         assert.equal(browser.asControl(listSelector).getProperty('headerText'), '...bites the dust!');
     });
@@ -56,6 +61,18 @@ describe('ui5 basics: properties and navigation', () => {
     });
 
     it('press an list item an show the name', () => {
+
+        const listItemSelector = {
+            selector: {
+                controlType: "sap.m.StandardListItem",
+                viewName: "test.Sample.view.Other"
+            }
+        }
+
+        const listitem = browser.asControl(listItemSelector)
+
+        // TODO:
+        // browser.asControl(listSelector).asControl ....
 
         // fire click event on a list item.
         // the event handler function checks the data:key property of the list item -> manually add this property to the event.
