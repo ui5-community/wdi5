47e4fa30105843f2394c94f83999447f47f96f1a
Volker Buzek <volker.buzek@js-soft.com>
2021-11-29 17:21:22 +0100

    feat: add color to console.log

---
+++ wdio-ui5-service/src/lib/coloredConsole.js
@@ -0,0 +1,30 @@
+const translate = (color) => {
+    switch (color) {
+        case 'red':
+            return '31';
+        case 'green':
+            return '32';
+        case 'yellow':
+            return '33';
+        case 'blue':
+            return '34';
+        case 'magenta':
+            return '35';
+        case 'cyan':
+            return '36';
+        default:
+            return '32'; // all is good
+    }
+};
+
+const colored =
+    (color) =>
+    (msg, ...other) =>
+        typeof msg === 'string'
+            ? console.log(`\x1b[${translate(color)}m%s\x1b[0m`, msg, ...other)
+            : console.log(msg, ...other);
+
+const _ = {};
+['red', 'green', 'yellow', 'blue', 'magenta', 'cyan'].forEach((color) => (_[color] = colored(color)));
+
+module.exports = Object.assign(console, _);
