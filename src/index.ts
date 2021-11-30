ea30572b6c51510ae20d5bab0058fc6995923693
Volker Buzek <volker.buzek@js-soft.com>
2021-11-30 21:02:07 +0100

    feat: initial plugin skeleton

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import Launcher from "./launcher"
+import Service from "./service"
+
+export default Service
+export const launcher = Launcher
---
+++ src/launcher.ts
@@ -0,0 +1,12 @@
+export default class Launcher {
+    // If a hook returns a promise, WebdriverIO will wait until that promise is resolved to continue.
+    async onPrepare(config, capabilities) {
+        // TODO: something before all workers launch
+    }
+
+    onComplete(exitCode, config, capabilities) {
+        // TODO: something after the workers shutdown
+    }
+
+    // custom service methods ...
+}
