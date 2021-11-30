8241edbbbd2173894ee2b566c09cd11cece5132c
Volker Buzek <volker.buzek@js-soft.com>
2021-11-30 22:31:33 +0100

    wip: describe type alias for browser wdi5 fns

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,4 +1,7 @@
 import { Path } from "typescript"
+import Log from "sap/base/Log"
+import RecordReplay from "sap/ui/test/RecordReplay"
+import { ControlSelector } from "sap/ui/test/RecordReplay"
 
 export type wdi5LogLevel = "silent" | "error" | "verbose"
 
@@ -8,5 +11,23 @@ export interface wdi5Config extends WebdriverIO.Config {
         url: string
         screenshotPath?: Path
         skipInjectUI5OnStart?: boolean
+    }
+}
+
+export interface wdi5Bridge extends Window {
+    bridge: RecordReplay
+    wdi5: {
+        createMatcher: (selector: ControlSelector) => ControlSelector
+        getUI5CtlForWebObj: (ui5Control: any) => any
+        retrieveControlMethods: (ui5Control: any) => string[]
+        isPrimitive: (value: any) => boolean
+        createControlIdMap: (ui5Controls: any[]) => Map<"id", string>
+        createControlId: (ui5Control: any) => { id: string }
+        isInitialized: boolean
+        Log: Log
+        waitForUI5Options: {
+            timeout: number
+            interval: number
+        }
     }
 }
