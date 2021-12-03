d51ee6c6cea348dfa84048bef91173acbac19cf9
Volker Buzek <volker.buzek@js-soft.com>
2021-12-03 23:23:57 +0100

    wip: test .asControl()

--- test/basics.test.ts
+++ test/basics.test.ts
@@ -1,8 +1,22 @@
-import { wdi5Config } from "../src/lib/types"
+import Button from "sap/m/Button"
+import { wdi5Selector } from "../src/types/wdi5.types"
+import {} from "../src/types/browser-commands"
 
-describe("basics", () => {
-    it("should have the right title", async () => {
+describe("basics", async () => {
+    it("should open a UI5-enabled site (here: DemoKit) supplied in wdio.conf's wdi5 setting", async () => {
         const title = await browser.getTitle()
         await expect(title).toEqual("OpenUI5 SDK - Demo Kit")
+    })
+
+    it("should find a ui5 control by id", async () => {
+        const selectorDownloadButton: wdi5Selector = {
+            selector: {
+                id: "readMoreButton",
+                controlType: "sap.m.Button",
+                viewName: "sap.ui.documentation.sdk.view.Welcome"
+            }
+        }
+        const controlDownloadButton: Button = await browser.asControl(selectorDownloadButton)
+        expect(controlDownloadButton.getText()).toEqual("Download")
     })
 })
