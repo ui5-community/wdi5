cc6d5fa334625e09d6597c8ee23cdf622dc7a7ef
Volker Buzek <volker.buzek@js-soft.com>
2021-12-22 15:35:56 +0100

    test: add regex locator suite

--- wdio-ui5-service/test/ui5-async.test.js
+++ wdio-ui5-service/test/ui5-async.test.js
@@ -220,3 +220,57 @@ describe('screenshots (async tests)', () => {
         }, 1500);
     });
 });
+
+describe('locate ui5 control via regex', () => {
+    /**
+     * click the version list button to open a popup on the sdk site
+     * then close it via "esc" key
+     * @param {String|RegExp} idRegex
+     */
+    async function _assert(idRegex) {
+        const selector = {
+            forceSelect: true, // make sure we're retrieving from scratch
+            selector: {
+                id: idRegex
+            }
+        };
+
+        if ((await browser.getUI5VersionAsFloat()) <= 1.6) {
+            selector.forceSelect = true;
+            selector.selector.interaction = 'root';
+        }
+
+        const button = await browser.asControl(selector);
+        await button.firePress();
+        const list = await browser.asControl(selectorList);
+        await expect(await list.getVisible()).toBeTruthy();
+
+        await browser.keys('Escape'); // close popup
+    }
+    it('plain regex /.../', async () => {
+        return await _assert(/.*changeVersionButton$/);
+    });
+
+    it('plain regex + flags /.../gmi', async () => {
+        return await _assert(/.*changeVersionButton$/gim);
+    });
+    it('new RegEx(/.../flags)', async () => {
+        return await _assert(new RegExp(/.*changeVersionButton$/));
+    });
+
+    it('new RegEx(/.../flags)', async () => {
+        return await _assert(new RegExp(/.*changeVersionButton$/gi));
+    });
+
+    it('new RegEx("string")', async () => {
+        return await _assert(new RegExp('.*changeVersionButton$'));
+    });
+
+    it('new RegEx("string", "flags")', async () => {
+        return await _assert(new RegExp('.*changeVersionButton$', 'gmi'));
+    });
+
+    it('regex shorthand matchers are handled properly', async () => {
+        return await _assert(/.*change\w.*Button$/);
+    });
+});
