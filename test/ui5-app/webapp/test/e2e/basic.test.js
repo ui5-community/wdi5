e5df2b9819044421ccef5788bd5258d506d2f4fd
Volker Buzek <volker.buzek@js-soft.com>
2021-12-29 13:44:23 +0100

    test: prep locating invisible controls

--- test/ui5-app/webapp/test/e2e/basic.test.js
+++ test/ui5-app/webapp/test/e2e/basic.test.js
@@ -13,6 +13,29 @@ describe('ui5 basic', () => {
         wdi5().getUtils().screenshot('test-basic');
     });
 
+    // TODO: make this work -> see #106
+    it.skip('should find a control with visible: false', () => {
+        const id = 'invisibleInputField';
+
+        // wdio-native selector
+        const wdioInput = browser.$(`[id$="${id}"]`);
+        expect(wdioInput.getProperty('id')).toContain('sap-ui-invisible');
+
+        const selector = {
+            selector: {
+                id,
+                controlType: 'sap.m.Input',
+                viewName: globalThis.viewName,
+                visible: false
+            }
+        };
+        const input = browser.asControl(selector);
+        expect(input.getVisible()).toBe(false);
+
+        input.setVisible(true);
+        expect(input.getVisible()).toBe(true);
+    });
+
     /*
      * It is important that we run each test in isolation. The running of a previous test
      * should not affect the next one. Otherwise, it could end up being very difficult to
