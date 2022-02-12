d7b8c73df4c013362c1984043064beb81aea4e78
Volker Buzek <volker.buzek@js-soft.com>
2022-02-12 21:36:16 +0100

    refactor: basic test

--- test/basics.test.ts
+++ test/basics.test.ts
@@ -5,7 +5,7 @@ import {} from "../src/types/browser-commands"
 describe("basics", async () => {
     it("should open a UI5-enabled site (here: DemoKit) supplied in wdio.conf's wdi5 setting", async () => {
         const title = await browser.getTitle()
-        await expect(title).toEqual("OpenUI5 SDK - Demo Kit")
+        await expect(title).toEqual("Demo Kit - OPENUI5 SDK")
     })
 
     it("should find a ui5 control by id", async () => {
@@ -17,6 +17,6 @@ describe("basics", async () => {
             }
         }
         const controlDownloadButton: Button = await browser.asControl(selectorDownloadButton)
-        expect(controlDownloadButton.getText()).toEqual("Download")
+        expect(await controlDownloadButton.getText()).toEqual("Download")
     })
 })
