e88b25af447f8af083985c3942ff41f5d2874020
Volker Buzek <volker.buzek@js-soft.com>
2022-02-15 21:40:54 +0100

    feat: expose wdi5 class

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import Launcher from "./launcher"
 import Service from "./service"
+import { wdi5 as _wdi5 } from "./wdi5"
 
 export default Service
 export const launcher = Launcher
+export const wdi5 = _wdi5
