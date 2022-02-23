794935e050d33bdc26fe08e97e98067148f38d1a
Volker Buzek <volker.buzek@js-soft.com>
2022-02-23 16:14:29 +0100

    test: add sHash obj parameter test

--- examples/ui5-js-app/webapp/test/e2e/hash-nav.test.js
+++ examples/ui5-js-app/webapp/test/e2e/hash-nav.test.js
@@ -1,5 +1,12 @@
 const { wdi5 } = require("wdio-ui5-service")
 
+const listSelector = {
+    selector: {
+        id: "PeopleList",
+        viewName: "test.Sample.view.Other"
+    }
+}
+
 describe("hash-based nav", () => {
     it('should allow the deep entry to "Other" view using wdi5 helper class and the UI5 router', async () => {
         const oRouteOptions = {
@@ -7,13 +14,6 @@ describe("hash-based nav", () => {
             sName: "RouteOther"
         }
         await wdi5.goTo("", oRouteOptions)
-
-        const listSelector = {
-            selector: {
-                id: "PeopleList",
-                viewName: "test.Sample.view.Other"
-            }
-        }
 
         if ((await browser.getUI5VersionAsFloat()) <= 1.6) {
             listSelector.forceSelect = true
@@ -41,19 +41,19 @@ describe("hash-based nav", () => {
         expect(await (await browser.asControl(buttonSelector)).getProperty("visible")).toBeTruthy()
     })
 
+    it("should navigate to Other view via sHash object parameter", async () => {
+        await wdi5.goTo({ sHash: "#/Other" })
+
+        const list = await browser.asControl(listSelector)
+        expect(await list.getVisible()).toBeTruthy()
+    })
+
     it('should allow the deep entry to "Other" view via the UI5 router directly', async () => {
         const oRouteOptions = {
             sComponentId: "container-Sample",
             sName: "RouteOther"
         }
         await browser.goTo({ oRoute: oRouteOptions })
-
-        const listSelector = {
-            selector: {
-                id: "PeopleList",
-                viewName: "test.Sample.view.Other"
-            }
-        }
 
         if ((await browser.getUI5VersionAsFloat()) <= 1.6) {
             listSelector.forceSelect = true
