bf02802186da4f3c3305f5fbca6af3072571fc13
Volker Buzek <volker.buzek@js-soft.com>
2022-02-24 18:08:06 +0100

    refactor: rm duplicate test cases

--- examples/ui5-js-app/webapp/test/e2e/basic.test.js
+++ examples/ui5-js-app/webapp/test/e2e/basic.test.js
@@ -8,65 +8,8 @@ describe("ui5 basic", () => {
         await Main.open()
     })
 
-    beforeEach(async () => {
-        wdi5.getLogger().log("beforeEach")
-        await browser.screenshot("test-basic")
-    })
-
-    // TODO: make this work -> see #106
-    it.skip("should find a control with visible: false", async () => {
-        const id = "invisibleInputField"
-
-        // wdio-native selector
-        const wdioInput = await browser.$(`[id$="${id}"]`)
-        expect(await wdioInput.getProperty("id")).toContain("sap-ui-invisible")
-
-        const selector = {
-            selector: {
-                id,
-                controlType: "sap.m.Input",
-                viewName: globalThis.viewName,
-                visible: false
-            }
-        }
-        const input = await browser.asControl(selector)
-        expect(await input.getVisible()).toBe(false)
-
-        await input.setVisible(true)
-        expect(await input.getVisible()).toBe(true)
-    })
-
     it("should have the right title", async () => {
         const title = await browser.getTitle()
         expect(title).toEqual("Sample UI5 Application")
-    })
-
-    it("should find a ui5 control class via .hasStyleClass", async () => {
-        // webdriver
-        const className = "myTestClass"
-
-        // ui5
-        const selector = {
-            wdio_ui5_key: "buttonSelector",
-            selector: {
-                bindingPath: {
-                    modelName: "testModel",
-                    propertyPath: "/buttonText"
-                },
-                viewName: globalThis.viewName,
-                controlType: "sap.m.Button"
-            }
-        }
-
-        if ((await browser.getUI5VersionAsFloat()) <= 1.6) {
-            selector.forceSelect = true
-            selector.selector.interaction = "root"
-        }
-
-        const control = await browser.asControl(selector)
-        const retrievedClassNameStatus = await control.hasStyleClass(className)
-
-        wdi5.getLogger().log("retrievedClassNameStatus", retrievedClassNameStatus)
-        expect(retrievedClassNameStatus).toBeTruthy()
     })
 })
