5859471dbeda2889c2c08abbcc1e004e5f88875d
Volker Buzek <volker.buzek@js-soft.com>
2022-02-25 10:31:57 +0100

    refactor: cleanup

--- examples/ui5-js-app/webapp/test/e2e/basic.test.js
+++ examples/ui5-js-app/webapp/test/e2e/basic.test.js
@@ -1,9 +1,6 @@
-const { wdi5 } = require("wdio-ui5-service")
 const Main = require("./pageObjects/Main")
 
 describe("ui5 basic", () => {
-    globalThis.viewName = "test.Sample.view.Main"
-
     before(async () => {
         await Main.open()
     })
