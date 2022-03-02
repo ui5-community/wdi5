448c3cf8649bb1c509170bfe61e09fd2c739ba73
Volker Buzek <volker.buzek@js-soft.com>
2022-03-02 17:23:51 +0100

    wip: crude fe api in wdi5 scope

--- examples/fe-app/app/incidents/webapp/wdi5-test/Journey.test.js
+++ examples/fe-app/app/incidents/webapp/wdi5-test/Journey.test.js
@@ -1,4 +1,10 @@
+const { wdi5 } = require("wdio-ui5-service")
+
 describe("FE basics", () => {
+    before(async () => {
+        await wdi5.goTo("#fe-lrop-v4")
+    })
+
     it("should trigger search on ListReport page", async () => {
         const FioriElementsFacade = await new browser.fe({
             ListReport: {
@@ -7,6 +13,9 @@ describe("FE basics", () => {
                 entitySet: "Incidents"
             }
         })
-        await FioriElementsFacade.onFilterBar().iExecuteSearch()
+        const search = await FioriElementsFacade.onFilterBar().iExecuteSearch()
+        const check = await FioriElementsFacade.onTable().iCheckRows()
+        expect(search).toBe(true)
+        expect(check).toBe(true)
     })
 })
--- src/lib/wdi5-fe.ts
+++ src/lib/wdi5-fe.ts
@@ -1,4 +1,6 @@
 import { clientSide_testLibrary } from "../../client-side-js/testLibrary"
+import { Logger as _Logger } from "./Logger"
+const Logger = _Logger.getInstance()
 
 export class WDI5FE {
     ListReport = {}
@@ -11,7 +13,29 @@ export class WDI5FE {
         return this
     }
 
+    onTable() {
+        return this
+    }
+
+    async iCheckRows() {
+        const result = await clientSide_testLibrary("ListReport", ["onTable", "iCheckRows"], this.ListReport, true)
+        if (result[0] === "error") {
+            Logger.error(result[1])
+            return false
+        } else {
+            Logger.success(result[1])
+            return true
+        }
+    }
+
     async iExecuteSearch() {
-        await clientSide_testLibrary("ListReport", ["onFilterBar", "iExecuteSearch"], this.ListReport)
+        const result = await clientSide_testLibrary("ListReport", ["onFilterBar", "iExecuteSearch"], this.ListReport)
+        if (result[0] === "error") {
+            Logger.error(result[1])
+            return false
+        } else {
+            Logger.success(result[1])
+            return true
+        }
     }
 }
