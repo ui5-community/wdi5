dc6afb9c5cb5544a0e8e8770ba2c62a9b841f51a
Volker Buzek <volker.buzek@js-soft.com>
2022-03-02 17:22:29 +0100

    wip: run sap.fe.test in mocked OPA context
    special props to Nicolas for jumpstarting :)
    
    Co-authored-by: Dominik Feininger <dominik.feininger@js-soft.com>
    Co-authored-by: Bischoff, Georg <georg.bischoff@sap.com>
    Co-authored-by: Simon Coen <Siolto@users.noreply.github.com>
    Co-authored-by: Nicolas Lunet <nicolas.lunet@sap.com>

--- client-side-js/testLibrary.js
+++ client-side-js/testLibrary.js
@@ -1,36 +1,68 @@
-async function clientSide_testLibrary(pageObject, eventName, oOptions) {
+async function clientSide_testLibrary(pageObject, aMethods, oOptions, isAssertion = false) {
     return await browser.executeAsync(
-        (pageObject, eventName, oOptions, done) => {
-            window.bridge.waitForUI5(window.wdi5.waitForUI5Options).then(() => {
-                window.wdi5.Log.info("[browser wdi5 FE] working " + eventName + " for FE page" + pageObject)
+        (pageObject, aMethods, oOptions, isAssertion, done) => {
+            window.bridge
+                .waitForUI5(window.wdi5.waitForUI5Options)
+                .then(() => {
+                    window.wdi5.Log.info(
+                        "[browser wdi5 FE] working " + aMethods.join(", ") + " for FE page" + pageObject
+                    )
 
-                const pageClass = new window.fe_bridge[pageObject](oOptions)
-
-                // DOM to ui5
-                let oControl = window.wdi5.getUI5CtlForWebObj(pageObject)
-                if (oControl && oControl.hasListeners(eventName)) {
-                    window.wdi5.Log.info("[browser wdi5] firing " + eventName + " on " + pageObject)
-                    // element existent and has the target event
-                    try {
-                        // eval the options indicated by option of type string
-                        if (typeof oOptions === "string") {
-                            oOptions = eval(oOptions)()
-                        }
-                        oControl.fireEvent(eventName, oOptions)
-                        // convert to boolean
-                        done(["success", true])
-                    } catch (e) {
-                        done(["error", e.toString()])
+                    // "cache" created page objects to avoid dups
+                    if (!window.wdi5.pageObjects) {
+                        window.wdi5.pageObjects = {}
                     }
-                } else {
-                    window.wdi5.Log.error("[browser wdi5] couldn't find " + pageObject)
-                    done(["error", false])
-                }
-            })
+                    let pageClass
+                    if (!window.wdi5.pageObjects[pageObject]) {
+                        pageClass = new window.fe_bridge[pageObject](oOptions, {
+                            // TODO: this will need to come via oOptions
+                            actions: {},
+                            assertions: {}
+                        })
+                        window.wdi5.pageObjects[pageObject] = pageClass
+                    } else {
+                        pageClass = window.wdi5.pageObjects[pageObject]
+                    }
+                    return pageClass
+                })
+                .then((pageClass) => {
+                    // put page into OPA queue
+                    // TODO: support page objects other than main page
+                    sap.ui.test.Opa5.createPageObjects({ onTheMainPage: pageClass })
+
+                    // mock the generic OK handler in order to support assertions
+                    sap.ui.test.Opa5.assert = {
+                        ok: function () {
+                            window.wdi5.Log.info("mocked assert!")
+                            return true
+                        }
+                    }
+
+                    const scope = isAssertion
+                        ? sap.ui.test.Opa.config.assertions.onTheMainPage
+                        : sap.ui.test.Opa.config.actions.onTheMainPage
+
+                    // execute all passed in methods
+                    aMethods.reduce((obj, methodName) => {
+                        return obj[methodName]()
+                    }, scope)
+
+                    // shoot off OPA queue execution
+                    return sap.ui.test.Opa.emptyQueue()
+                })
+                .then(() => {
+                    done(["success", true])
+                })
+                .catch((err) => {
+                    debugger
+                    window.wdi5.Log.error(err)
+                    done(["error", err.toString()])
+                })
         },
         pageObject,
-        eventName,
-        oOptions
+        aMethods,
+        oOptions,
+        isAssertion
     )
 }
 
