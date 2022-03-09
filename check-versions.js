a687e58d7ce1f19bc4a264742b7a9d48c3cff708
dominik.feininger <dominik.feininger@js-soft.com>
2022-03-09 12:56:38 +0100

    chore(npm): enforcing npm version

---
+++ check-versions.js
@@ -0,0 +1,28 @@
+const exec = require("child_process").exec
+const execSync = require("child_process").execSync
+
+exec("npm -v", function (error, stdout, stderr) {
+    "use strict"
+    if (error) {
+        console.error("error: " + error + " " + stderr)
+        return
+    }
+
+    let sReqNpmVersion = process.env.npm_package_engines_npm
+    const npmVersion = parseFloat(stdout)
+
+    sReqNpmVersion = sReqNpmVersion.replace("<", "")
+    sReqNpmVersion = sReqNpmVersion.replace("=", "")
+    sReqNpmVersion = sReqNpmVersion.replace(">", "")
+    const fReqNpmVersion = parseFloat(sReqNpmVersion)
+
+    if (fReqNpmVersion > npmVersion) {
+        console.info(
+            `updating npm version from ${npmVersion} to the lasted version because required npm version in package.json is set to ${requiredNpmVersion}`
+        )
+        execSync("npm install -g npm@latest")
+        execSync("npm install")
+    } else {
+        execSync("npm install")
+    }
+})
