31899d6953b94ea381bbf72b0c7dc35d64f9651b
Kireilis, Edvardas <edvardas.kireilis@sap.com>
2022-04-01 11:30:06 +1300

    fix: merge issue resolved

--- client-side-js/interactWithControl.js
+++ client-side-js/interactWithControl.js
@@ -5,18 +5,21 @@ async function clientSide_interactWithControl(oOptions) {
             done(["error", error.toString()])
         }
 
-        window.wdi5.waitForUI5(window.wdi5.waitForUI5Options, () => {
-            window.wdi5.Log.info("[browser wdi5] locating controlSelector")
-            oOptions.selector = window.wdi5.createMatcher(oOptions.selector)
-            window.bridge
+        window.wdi5.waitForUI5(
+            window.wdi5.waitForUI5Options,
+            () => {
+                window.wdi5.Log.info("[browser wdi5] locating controlSelector")
+                oOptions.selector = window.wdi5.createMatcher(oOptions.selector)
+                window.bridge
                     .interactWithControl(oOptions)
                     .then((result) => {
                         window.wdi5.Log.info("[browser wdi5] interaction complete! - Message: " + result)
                         done(["success", result])
                     })
                     .catch(errorHandling)
-            }
-        }, errorHandling)
+            },
+            errorHandling
+        )
     }, oOptions)
 }
 
