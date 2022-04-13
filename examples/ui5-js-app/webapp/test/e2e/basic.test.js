218d28df993de6544690eb41a8883415ec622c60
dominik.feininger <dominik.feininger@js-soft.com>
2022-04-13 15:14:37 +0200

    fix: rm only option in basic test

--- examples/ui5-js-app/webapp/test/e2e/basic.test.js
+++ examples/ui5-js-app/webapp/test/e2e/basic.test.js
@@ -62,7 +62,7 @@ describe("ui5 basic", () => {
         expect(await browser.asControl(searchFieldSelector).getValue()).toEqual("search Value")
     }) */
 
-    it.only("check binding info", async () => {
+    it("check binding info", async () => {
         const title = await browser.asControl({ selector: { id: "container-Sample---Main--Title::NoAction.h1" } })
         const bindingInfo = await title.getBinding("text")
         expect(bindingInfo.oValue).toEqual("UI5 demo")
