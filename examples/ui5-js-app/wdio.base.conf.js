ff90430b121a0249a02fc607add3bfe5ae89cbd9
dominik.feininger <dominik.feininger@js-soft.com>
2022-04-20 15:50:59 +0200

    refactor: config base to export config

--- examples/ui5-js-app/wdio.base.conf.js
+++ examples/ui5-js-app/wdio.base.conf.js
@@ -1,6 +1,6 @@
 const { join } = require("path")
 
-exports.baseConfig = {
+exports.config = {
     wdi5: {
         screenshotPath: join("webapp", "test", "__screenshots__"),
         logLevel: "error"
