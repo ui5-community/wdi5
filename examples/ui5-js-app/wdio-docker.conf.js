c5236ee5ccc0b571a012f5d53f87b5a8f1de339e
dominik.feininger <dominik.feininger@js-soft.com>
2022-04-25 16:35:22 +0200

    feat(firefox): wip add firefox (not executing tests)

--- examples/ui5-js-app/wdio-docker.conf.js
+++ examples/ui5-js-app/wdio-docker.conf.js
@@ -6,7 +6,7 @@ exports.config = {
     path: "/wd/hub",
     maxInstances: 1,
     capabilities: [
-        {
+        /* {
             maxInstances: 1,
             browserName: "chrome",
             acceptInsecureCerts: true,
@@ -27,6 +27,25 @@ exports.config = {
                     "--ignore-certificate-errors"
                 ]
             }
+        }, */
+        {
+            // maxInstances can get overwritten per capability. So if you have an in house Selenium
+            // grid with only 5 firefox instance available you can make sure that not more than
+            // 5 instance gets started at a time.
+            maxInstances: 1,
+            browserName: "firefox",
+            specs: ["test/ffOnly/*"],
+            "moz:firefoxOptions": {
+                // flag to activate Firefox headless mode (see https://github.com/mozilla/geckodriver/blob/master/README.md#firefox-capabilities for more details about moz:firefoxOptions)
+                args: ["-headless"]
+            },
+            // If outputDir is provided WebdriverIO can capture driver session logs
+            // it is possible to configure which logTypes to exclude.
+            // excludeDriverLogs: ['*'], // pass '*' to exclude all driver session logs
+            excludeDriverLogs: ["bugreport", "server"]
+            //
+            // Parameter to ignore some or all Puppeteer default arguments
+            // ignoreDefaultArgs: ['-foreground'], // set value to true to ignore all default arguments
         }
     ],
     wdi5: {
