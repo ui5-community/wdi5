8a0c55531979ca43b4b869ac1b341f198823f875
dominik.feininger <dominik.feininger@js-soft.com>
2022-05-06 09:31:41 +0200

    feat(bs): add browsers env varaiable

--- examples/ui5-js-app/wdio-browserstack.conf.js
+++ examples/ui5-js-app/wdio-browserstack.conf.js
@@ -23,43 +23,67 @@ let conf = merge(baseConfig, _config)
 
 // override, not merge
 conf.services = [["browserstack", { browserstackLocal: true }], "ui5"]
-conf.capabilities = [
-    {
-        browserName: "Chrome",
-        browserVersion: "latest",
-        "bstack:options": {
-            os: "OS X",
-            osVersion: "Monterey"
-        }
-    },
-    {
-        browserName: "Firefox",
-        browserVersion: "98.0",
-        "bstack:options": {
-            os: "Windows",
-            osVersion: "11",
-            local: true,
-            networkLogs: true,
-            seleniumVersion: "3.10.0",
-            video: false
-        }
-    },
-    {
-        browserName: "Safari",
-        browserVersion: "latest",
-        "bstack:options": {
-            os: "OS X",
-            osVersion: "Monterey"
-        }
-    },
-    {
-        browserName: "Edge",
-        browserVersion: "latest",
-        "bstack:options": {
-            os: "Windows",
-            osVersion: "11"
-        }
+
+// browsers
+const chrome = {
+    browserName: "Chrome",
+    browserVersion: "latest",
+    "bstack:options": {
+        os: "OS X",
+        osVersion: "Monterey"
     }
-]
+}
+const firefox = {
+    browserName: "Firefox",
+    browserVersion: "98.0",
+    "bstack:options": {
+        os: "Windows",
+        osVersion: "11",
+        local: true,
+        networkLogs: true,
+        seleniumVersion: "3.10.0",
+        video: false
+    }
+}
+const safari = {
+    browserName: "Safari",
+    browserVersion: "latest",
+    "bstack:options": {
+        os: "OS X",
+        osVersion: "Monterey"
+    }
+}
+const edge = {
+    browserName: "Edge",
+    browserVersion: "latest",
+    "bstack:options": {
+        os: "Windows",
+        osVersion: "11"
+    }
+}
+
+// add browsers
+conf.capabilities = []
+const _browsers = process.env.BROWSERS
+if (_browsers && _browsers.length > 0) {
+    console.log(`requested browsers: ${_browsers}`)
+    if (_browsers.includes("chrome")) {
+        conf.capabilities.push(chrome)
+    }
+    if (_browsers.includes("safari")) {
+        conf.capabilities.push(safari)
+    }
+    if (_browsers.includes("firefox")) {
+        conf.capabilities.push(firefox)
+    }
+    if (_browsers.includes("edge")) {
+        conf.capabilities.push(edge)
+    }
+} else {
+    conf.capabilities.push(chrome)
+    conf.capabilities.push(safari)
+    conf.capabilities.push(firefox)
+    conf.capabilities.push(edge)
+}
 
 exports.config = conf
