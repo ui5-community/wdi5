1b3a3f4e1cd2432aeebff83305023d1a6de5587a
dominik.feininger <dominik.feininger@js-soft.com>
2022-05-12 16:27:06 +0200

    chore(github): fix test run

--- examples/ui5-js-app/wdio-bs-btp.conf.js
+++ examples/ui5-js-app/wdio-bs-btp.conf.js
@@ -5,7 +5,6 @@ const bs_conf = require("./wdio-browserstack.conf")
 const { join } = require("path")
 
 const _config = {
-    specs: [join("webapp", "test", "e2e", "basic.test.js")],
     baseUrl:
         "https://davinci.cpp.cfapps.eu10.hana.ondemand.com/474a7c0c-c364-4075-b53e-983472d76120.basicservice.testSample-1.0.0/index.html"
 }
