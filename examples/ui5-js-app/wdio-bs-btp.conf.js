0758ee849d153404fb87a66ca57102a0199100ba
dominik.feininger <dominik.feininger@js-soft.com>
2022-05-16 14:13:56 +0200

    wip(bs): set max browser to 4

--- examples/ui5-js-app/wdio-bs-btp.conf.js
+++ examples/ui5-js-app/wdio-bs-btp.conf.js
@@ -2,9 +2,9 @@ const path = require("path")
 require("dotenv").config({ path: path.resolve(__dirname, "../../.env") })
 const merge = require("deepmerge")
 const bs_conf = require("./wdio-browserstack.conf")
-const { join } = require("path")
 
 const _config = {
+    maxInstances: 4,
     baseUrl:
         "https://davinci.cpp.cfapps.eu10.hana.ondemand.com/474a7c0c-c364-4075-b53e-983472d76120.basicservice.testSample-1.0.0/index.html"
 }
