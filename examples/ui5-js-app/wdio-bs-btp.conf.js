965463afe5ae6863fcbc0d95c26bebcf0aca3126
dominik.feininger <dominik.feininger@js-soft.com>
2022-05-17 09:26:06 +0200

    feat(bs): enable network logs

--- examples/ui5-js-app/wdio-bs-btp.conf.js
+++ examples/ui5-js-app/wdio-bs-btp.conf.js
@@ -5,6 +5,7 @@ const bs_conf = require("./wdio-browserstack.conf")
 
 const _config = {
     maxInstances: 4,
+    networkLogs: "true",
     baseUrl:
         "https://davinci.cpp.cfapps.eu10.hana.ondemand.com/474a7c0c-c364-4075-b53e-983472d76120.basicservice.testSample-1.0.0/index.html"
 }
