64ae78b0530aedb0bb6384241cd44d0e28afa2af
dominik.feininger <dominik.feininger@js-soft.com>
2022-05-18 09:46:00 +0200

    feat(bs): single instance only

--- examples/ui5-js-app/wdio-bs-btp.conf.js
+++ examples/ui5-js-app/wdio-bs-btp.conf.js
@@ -4,8 +4,7 @@ const merge = require("deepmerge")
 const bs_conf = require("./wdio-browserstack.conf")
 
 const _config = {
-    maxInstances: 4,
-    // networkLogs: "true",
+    maxInstances: 1,
     baseUrl:
         "https://davinci.cpp.cfapps.eu10.hana.ondemand.com/474a7c0c-c364-4075-b53e-983472d76120.basicservice.testSample-1.0.0/index.html"
 }
