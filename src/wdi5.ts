1d1262381a04a4df6d87fd529ff31888418f9498
Volker Buzek <volker.buzek@js-soft.com>
2022-07-05 23:17:20 +0200

    refactor: wdi5.goTo(...) (#303)
    properly define overloads for different params supplied to .goTo(),
    encouraging usage of a single one (`wdi5.goTo(param)`), 
    with param being either 
    - a string
    - an object containing an sHash property or 
    - a UI5 router object
    the deprecated 2 parameter signature is still supported, but logged as a warning.
    also, documentation was updated to reflect above change.

--- src/wdi5.ts
+++ src/wdi5.ts
@@ -4,14 +4,56 @@ export class wdi5 {
         return Logger.getInstance(sPrefix)
     }
 
-    static async goTo(param: any, oRoute, browserInstance: WebdriverIO.Browser = browser) {
-        if (param) {
-            Logger.getInstance().log(`Navigating to: ${JSON.stringify(param)}`)
-            await browserInstance.goTo(param)
+    /**
+     * navigate to a route/view of a UI5 app - by router object
+     *
+     * @param routerOption a UI5 router object, e.g. {
+        sComponentId,
+        sName,
+        oParameters,
+        oComponentTargetInfo,
+        bReplace
+    }
+     * @param browserInstance the currently remote controlled browser
+     */
+    static async goTo(routerOption: any, browserInstance?: WebdriverIO.Browser)
+    /**
+     * navigate to a route/view of a UI5 app - by router object
+     *
+     * @param withSHash a UI5 router object, e.g. {sHash:"#/accounts/create"}
+     * @param browserInstance the currently remote controlled browser
+     */
+    static async goTo(withSHash: any, browserInstance?: WebdriverIO.Browser)
+    /**
+     * navigate to a route/view of a UI5 app - by string hash
+     *
+     * @hash hash hash-part of the URL, e.g. "#/accounts/create"
+     * @param browserInstance the currently remote controlled browser
+     */
+    static async goTo(hash: string, browserInstance?: WebdriverIO.Browser)
+    /**
+     * @deprecated please supply only a single parameter to .goTo() and optionally a browser instance
+     */
+    static async goTo(param: any, oRoute: any, browserInstance?: WebdriverIO.Browser)
+    static async goTo(byWhat, oRoute: any, browserInstance: WebdriverIO.Browser = browser) {
+        if (oRoute) {
+            Logger.getInstance().warn(
+                "deprecated signature: please use single parameter as nav target: wdi5.goTo(target)"
+            )
+            byWhat = oRoute
+        }
+        if (typeof byWhat === "string") {
+            Logger.getInstance().log(`Navigating via string hash: ${byWhat}`)
+            await browserInstance.goTo(byWhat)
+        } else if (typeof byWhat === "object" && byWhat.sHash) {
+            Logger.getInstance().log(`Navigating via object w/ property sHash: ${JSON.stringify(byWhat)}`)
+            await browserInstance.goTo(byWhat)
+        } else if (typeof byWhat === "object") {
+            Logger.getInstance().log(`Navigating via UI5 router object: ${JSON.stringify(byWhat)}`)
+            await browserInstance.goTo({ oRoute: byWhat })
         } else {
-            Logger.getInstance().log(`Navigating to: ${JSON.stringify(oRoute)}`)
-            // ui5 router based navigation
-            await browserInstance.goTo({ oRoute })
+            Logger.getInstance().log(`Navigating via generic object: ${JSON.stringify(byWhat)}`)
+            await browserInstance.goTo({ byWhat })
         }
     }
 }
