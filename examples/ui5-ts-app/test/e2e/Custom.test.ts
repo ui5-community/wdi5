e40cbcb8a9cb20841ca114609daae0bea7ee9cef
Volker Buzek <volker.buzek@js-soft.com>
2022-11-11 15:52:32 +0100

    test(auth,custom): meaningful expect

--- examples/ui5-ts-app/test/e2e/Custom.test.ts
+++ examples/ui5-ts-app/test/e2e/Custom.test.ts
@@ -1,5 +1,9 @@
-describe("Basic", async () => {
-    it("browser.allControls: check number of buttons", async () => {
-        expect(await $("#flash").isDisplayed()).toBeTruthy()
+describe("custom login", async () => {
+    // we silently acknowledge that this is not a UI5 app
+    // being tested, so browser-scoped errors are ok
+    it("w/ a username + password form", async () => {
+        const expectedSuccessMessage = "You logged into a secure area!"
+        const actualSuccessMessage = await $("#flash").getText()
+        expect(actualSuccessMessage).toContain(expectedSuccessMessage)
     })
 })
