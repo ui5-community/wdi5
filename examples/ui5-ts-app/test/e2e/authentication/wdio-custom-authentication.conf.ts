5fd58bef34bfa818170193910ce84d6b75d7e8db
Volker Buzek <volker.buzek@js-soft.com>
2022-11-11 15:51:52 +0100

    refactor(auth,custom): simplify to use base conf

--- examples/ui5-ts-app/test/e2e/authentication/wdio-custom-authentication.conf.ts
+++ examples/ui5-ts-app/test/e2e/authentication/wdio-custom-authentication.conf.ts
@@ -1,53 +1,36 @@
-import { join } from "path"
-import { wdi5Config } from "wdio-ui5-service/dist/types/wdi5.types"
+import { config as baseConf } from "./wdio-base.conf"
+import { wdi5Capabilites } from "wdio-ui5-service/dist/types/wdi5.types"
+import merge from "ts-deepmerge"
+
+// hack-y in these public (!) credentials
 process.env.wdi5_username = "tomsmith"
 process.env.wdi5_password = "SuperSecretPassword!"
-export const config: wdi5Config = {
-    wdi5: {
-        screenshotPath: join("test", "__screenshots__"),
-        url: "",
-        waitForUI5Timeout: 30000
+
+const capability: wdi5Capabilites = {
+    "wdi5:authentication": {
+        provider: "custom",
+        usernameSelector: "input[id='username']",
+        passwordSelector: "input[id='password']",
+        submitSelector: "button[type='submit']"
     },
-    baseUrl: "https://the-internet.herokuapp.com/login",
-
-    services: ["chromedriver", "ui5"],
-
-    specs: ["./test/e2e/**/Custom.test.ts"],
-
-    maxInstances: 10,
-    capabilities: [
-        {
-            "wdi5:authentication": {
-                provider: "custom",
-                usernameSelector: "input[id='username']",
-                passwordSelector: "input[id='password']",
-                submitSelector: "button[type='submit']"
-            },
-            maxInstances: 5,
-            browserName: "chrome",
-            "goog:chromeOptions": {
-                args:
-                    process.argv.indexOf("--headless") > -1
-                        ? ["--headless"]
-                        : process.argv.indexOf("--debug") > -1
-                        ? ["window-size=1440,800", "--auto-open-devtools-for-tabs"]
-                        : ["window-size=1440,800"]
-            },
-            acceptInsecureCerts: true
-        }
-    ],
-    logLevel: "error",
-    bail: 0,
-
-    waitforTimeout: 10000,
-    connectionRetryTimeout: process.argv.indexOf("--debug") > -1 ? 1200000 : 120000,
-    connectionRetryCount: 3,
-
-    reporters: ["spec"],
-
-    framework: "mocha",
-    mochaOpts: {
-        ui: "bdd",
-        timeout: process.argv.indexOf("--debug") > -1 ? 600000 : 60000
-    }
+    maxInstances: 5,
+    browserName: "chrome",
+    "goog:chromeOptions": {
+        args:
+            process.argv.indexOf("--headless") > -1
+                ? ["--headless"]
+                : process.argv.indexOf("--debug") > -1
+                ? ["window-size=1440,800", "--auto-open-devtools-for-tabs"]
+                : ["window-size=1440,800"]
+    },
+    acceptInsecureCerts: true
 }
+const _config = {
+    // we silently acknowledge that this is not a UI5 app
+    // being tested, so browser-scoped errors are ok
+    baseUrl: "https://the-internet.herokuapp.com/login",
+    capabilities: [capability]
+}
+const config = merge(baseConf, _config)
+config.specs = ["./test/e2e/**/Custom.test.ts"]
+export { config }
