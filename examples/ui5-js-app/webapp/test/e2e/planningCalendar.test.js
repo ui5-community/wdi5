adaa0a1dd63cbad4f0cac82ee00dc6490a766671
dominikfeininger <dominik.feininger@js-soft.com>
2022-11-16 10:13:40 +0100

    test(planningcalendar): create working test

--- examples/ui5-js-app/webapp/test/e2e/planningCalendar.test.js
+++ examples/ui5-js-app/webapp/test/e2e/planningCalendar.test.js
@@ -13,15 +13,30 @@ describe("Planning Calendar test spec", () => {
         const pc = await CL.getPlanningCalendar()
         // I get aggregated rows
         const rows = await pc.getRows()
+        // each people has a row
+
+        const cells0 = await rows[0].getCells()
+        // the first two rows have two cell which are the the reminders and the appointments, third row does not have reminders!
+        const appointments0 = await cells0[1].getAppointments()
+        expect(appointments0.length).toBeGreaterThan(4) // four appointments visible on the selected date, but 22 in total
+
+        // every first appointment is a meeting
+        // the first three appointment are in the past and not visible -> not in DOM -> see error logs
+        const appointments0Text = await appointments0[3].getTitle()
+        expect(appointments0Text).toContain("Meet Max Mustermann")
+
+        const cells1 = await rows[1].getCells()
+        // the first two rows have two cell which are the the reminders and the appointments, third row does not have reminders!
+        const appointments1 = await cells1[1].getAppointments()
+        expect(appointments1.length).toBeGreaterThan(4) // three appointments visible on the selected date, but 11 in total
+
+        // every first appointment is a meeting
+        // the first three appointment are in the past and not visible -> not in DOM -> see error logs
+        const appointments1Text = await appointments1[3].getTitle()
+        expect(appointments1Text).toContain("Team meeting")
+
+        // test in all rows
         for (const row of rows) {
-            const cells = await row.getCells()
-            const appointments = await cells[1].getAppointments()
-            expect(appointments.length).toBeGreaterThan(4)
-
-            // every first appointment is a meeting
-            const appointmentText = await appointments[0].getTitle()
-            expect(appointmentText).toContain("Meet")
-
             const rowInfo = await row.getControlInfo()
             expect(rowInfo.className).toBe("sap.m.internal.PlanningCalendarRowListItem")
         }
