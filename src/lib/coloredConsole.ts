acc4086950b8ddfdcaaf918b00d52be400f42571
Simon Coen <32054457+Siolto@users.noreply.github.com>
2022-12-13 08:40:29 +0100

    fix: resolve cucumber namespace conflict  (#382)
    closes #348 
    context: when wdi5 is used with cucumber instead of mocca, node runtime dumped during test execution.
    this was due to cucumber using the npm module "console" which caused a namespace/scope conflict w/ 
    wdi5's own enhancement of the global console object
    * refactor: use dynamic import depending on node version
    * refactor: console scope
    
    Co-authored-by: Volker Buzek <volker.buzek@js-soft.com>

--- src/lib/coloredConsole.ts
+++ src/lib/coloredConsole.ts
@@ -1,3 +1,15 @@
+// the cucumber framework uses the 'console' module which also modifies the console object.
+// We need to import the console specifically otherwise we run into duplicate names and node
+// will kill the whole processes.
+
+const node14Up = parseInt(process.versions.node.substring(0, 2)) > 14
+let console = global.console
+if (node14Up) {
+    ;(async () => {
+        console = await import("node:console")
+    })()
+}
+
 const translate = (color) => {
     switch (color) {
         case "red":
@@ -35,5 +47,5 @@ const _ = {
     cyan: colored("cyan"),
     default: colored("default")
 }
-
+// we need to use a copy of the original object as 'coloredConsole' is a frozen object
 export const chalk = Object.assign(console, { ..._ })
