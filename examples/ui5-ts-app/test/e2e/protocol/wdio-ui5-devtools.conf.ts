012a74c426b0b7d85df5d014ffd274c34622e44b
Adrianna Böhmert <adrianna.boehmert@sap.com>
2023-01-18 12:53:46 +0100

    test(ts): for devtools protocol

---
+++ examples/ui5-ts-app/test/e2e/protocol/wdio-ui5-devtools.conf.ts
@@ -0,0 +1,19 @@
+import { wdi5Config } from "wdio-ui5-service/dist/types/wdi5.types"
+
+export const config: wdi5Config = {
+    baseUrl: "https://cswdev.launchpad.cfapps.eu10.hana.ondemand.com/site/csw#TestSampleTSapp-display",
+    services: ["ui5"] /* no drivers, so wdio is falling back to devtools*/,
+    specs: ["test/e2e/protocol/Protocol.test.ts"],
+    capabilities: [
+        {
+            browserName: "chrome"
+        }
+    ],
+    waitforTimeout: 20000,
+    logLevel: "debug",
+    reporters: ["spec"],
+    framework: "mocha",
+    mochaOpts: {
+        timeout: 20000
+    }
+}
