9566ad77aaae9ded7a032e28a5af809e2a6539d5
Volker Buzek <volker.buzek@js-soft.com>
2023-01-25 10:34:48 +0100

    refactor(ts,test): for readability

--- examples/ui5-ts-app/test/e2e/Input.test.ts
+++ examples/ui5-ts-app/test/e2e/Input.test.ts
@@ -1,27 +1,31 @@
 import Input from "sap/m/Input"
 import { wdi5Selector } from "wdio-ui5-service/dist/types/wdi5.types"
 
+const inputSelector: wdi5Selector = {
+    selector: {
+        id: "mainUserInput",
+        viewName: "test.Sample.tsapp.view.Main"
+    }
+}
+
 describe("Input", async () => {
     it("should read name from username field", async () => {
-        const inputText: wdi5Selector = {
-            selector: {
-                id: "mainUserInput",
-                viewName: "test.Sample.tsapp.view.Main"
-            }
-        }
-        const inputString: string = await (browser.asControl(inputText) as unknown as Input).getValue()
-        expect(inputString).toEqual("Helvetius Nagy")
+        const input: unknown = await browser.asControl(inputSelector)
+        const value: string = await (input as Input).getValue()
+        expect(value).toEqual("Helvetius Nagy")
     })
 
     it("should check if the field is writeable", async () => {
-        const inputText: wdi5Selector = {
-            selector: {
-                id: "mainUserInput",
-                viewName: "test.Sample.tsapp.view.Main"
-            }
-        }
-        await (browser.asControl(inputText) as unknown as Input).setValue("Smith Smithersson")
-        const input = await (browser.asControl(inputText) as unknown as Input).getValue()
-        expect(input).toEqual("Smith Smithersson")
+        const newValue = "DSAG UI expert round"
+        await (browser.asControl(inputSelector) as unknown as Input).setValue(newValue)
+        const input = await (browser.asControl(inputSelector) as unknown as Input).getValue()
+        expect(input).toEqual(newValue)
+    })
+
+    it.only("should retrieve the webcomponent's bound path via a managed object", async () => {
+        const control = await browser.asControl(inputSelector)
+        const bindingInfo = await control.getBindingInfo("value")
+        const parts = await bindingInfo.parts
+        expect(parts[0].path).toEqual("/Customers('TRAIH')/ContactName")
     })
 })
