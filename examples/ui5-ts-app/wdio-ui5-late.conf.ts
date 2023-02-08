df4e70fca32d1d1e2cc82cc47db483697da1b622
Volker Buzek <volker.buzek@js-soft.com>
2023-02-08 19:32:30 +0100

    test(ts): for late injecting wdi5

---
+++ examples/ui5-ts-app/wdio-ui5-late.conf.ts
@@ -0,0 +1,7 @@
+import { config } from "./wdio-ui5.conf"
+
+config.wdi5 = { skipInjectUI5OnStart: true }
+config.specs = ["./test/e2e/ui5-late.test.ts"]
+config.baseUrl = "https://github.com/ui5-community/wdi5/"
+
+exports.config = config
