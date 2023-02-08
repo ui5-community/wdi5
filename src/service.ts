67bdff744d59c2551dcdc16a1026093a096fccee
Volker Buzek <volker.buzek@js-soft.com>
2023-02-08 19:31:35 +0100

    revert: wdio inits the service
    but make props optional for proper devx

--- src/service.ts
+++ src/service.ts
@@ -1,4 +1,4 @@
-import { Services } from "@wdio/types"
+import { Capabilities, Services } from "@wdio/types"
 import { MultiRemoteDriver } from "webdriverio/build/multiremote"
 
 import { start, injectUI5, setup, checkForUI5Page, authenticate } from "./lib/wdi5-bridge"
@@ -8,13 +8,11 @@ import { Logger as _Logger } from "./lib/Logger"
 const Logger = _Logger.getInstance()
 
 export default class Service implements Services.ServiceInstance {
-    private _config: any
-    private _capabilities: any
-
-    constructor() {
-        this._config = {}
-        this._capabilities = {}
-    }
+    constructor(
+        private _options?: wdi5Config, // TODO: this is the successor to _config in wdio^8
+        private _capabilities?: Capabilities.RemoteCapability,
+        private _config?: wdi5Config // an enhanced version of the regular wdio config
+    ) {} // the Service is instantiated by wdio with the capabilites and config passed on to
 
     async before(/*capabilities* , specs*/) {
         // if no wdi5 config is available we add it manually
