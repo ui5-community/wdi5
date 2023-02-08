bde49d11cad0da58897326978c9fc65b6fd565ea
Volker Buzek <volker.buzek@js-soft.com>
2023-02-08 21:04:12 +0100

    feat: allow for async client-side controller functions (#423)
    * detect client async fn and await them
    * cater towards throwing async fn client-side
    fixes #417

--- client-side-js/executeObjectMethod.js
+++ client-side-js/executeObjectMethod.js
@@ -3,16 +3,35 @@ async function clientSide_executeObjectMethod(uuid, methodName, args) {
         (uuid, methodName, args, done) => {
             window.wdi5.waitForUI5(
                 window.wdi5.waitForUI5Options,
-                () => {
+                // this callback is denoted "async" even though it is truely not
+                // but what other way to `await` a potentially async UI5 managed object fn in here?
+                async () => {
                     // DOM to UI5
                     const oObject = window.wdi5.objectMap[uuid]
 
                     // execute the function
                     // TODO: if (methodName === "getName") { debugger }
-                    let result = oObject[methodName].apply(oObject, args)
+                    let result
+                    let threw = false
+                    let threwMessage = ""
+                    if (oObject[methodName].constructor.name === "AsyncFunction") {
+                        try {
+                            result = await oObject[methodName].apply(oObject, args)
+                        } catch (error) {
+                            threw = true
+                            threwMessage = JSON.stringify(error)
+                            window.wdi5.Log.error(threwMessage)
+                        }
+                    } else {
+                        result = oObject[methodName].apply(oObject, args)
+                    }
 
+                    // async message call rejected
+                    if (threw) {
+                        done({ status: 1, message: threwMessage })
+                    }
                     // result mus be a primitive
-                    if (window.wdi5.isPrimitive(result)) {
+                    else if (window.wdi5.isPrimitive(result)) {
                         // getter
                         done({ status: 0, result: result, returnType: "result" })
                     } else {
