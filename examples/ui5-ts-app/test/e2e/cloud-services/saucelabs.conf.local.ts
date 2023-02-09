5fe9d0145a4a4c4e305f462b408238a999a8a942
Volker Buzek <volker.buzek@js-soft.com>
2023-02-09 10:46:30 +0100

    wip: saucelabs integration

---
+++ examples/ui5-ts-app/test/e2e/cloud-services/saucelabs.conf.local.ts
@@ -0,0 +1,115 @@
+import * as dotenv from "dotenv"
+dotenv.config()
+
+import { config as shared } from "./shared.conf"
+
+const _ts = new Date()
+const ts = `${_ts.getFullYear()}-${_ts.getMonth() + 1}-${
+    _ts.getDay() + 1
+}_${_ts.getHours()}:${_ts.getMinutes()}:${_ts.getSeconds()}`
+
+const defaultsWin = {
+    name: "wdi5-ts-app",
+    build: `${process.env.USER}::${ts}`,
+    screenResolution: "1400x1050",
+    extendedDebugging: true
+}
+const defaultsMac = {
+    name: "wdi5-ts-app",
+    build: `${process.env.USER}::${ts}`,
+    screenResolution: "1440x900",
+    extendedDebugging: true
+}
+const acceptInsecureCerts = true
+
+const _config = {
+    connectionRetryTimeout: 10000,
+    connectionRetryCount: 1,
+
+    services: [["sauce", { sauceConnect: true }], "ui5"],
+    user: process.env.SAUCE_USERNAME,
+    key: process.env.SAUCE_ACCESS_KEY,
+    region: "eu",
+
+    maxInstances: 10,
+    maxInstancesPerCapability: 1,
+    capabilities: [
+        {
+            browserName: "chrome",
+            browserVersion: "latest",
+            platformName: "Windows 10",
+            acceptInsecureCerts,
+            "sauce:options": {
+                ...defaultsWin
+            }
+        }
+        // Windows
+        // {
+        //     browserName: "MicrosoftEdge",
+        //     browserVersion: "latest",
+        //     platformName: "Windows 11",
+        //     acceptInsecureCerts,
+        //     "sauce:options": {
+        //         ...defaultsWin
+        //     }
+        // }
+        // ,
+        // {
+        //     browserName: "chrome",
+        //     browserVersion: "latest",
+        //     platformName: "Windows 11",
+        //     acceptInsecureCerts,
+        //     "sauce:options": {
+        //         ...defaultsWin
+        //     }
+        // },
+        // {
+        //     browserName: "firefox",
+        //     browserVersion: "latest",
+        //     platformName: "Windows 11",
+        //     acceptInsecureCerts,
+        //     "sauce:options": {
+        //         ...defaultsWin
+        //     }
+        // },
+        // // macOS
+        // {
+        //     browserName: "safari",
+        //     browserVersion: "latest",
+        //     platformName: "macOS 13",
+        //     acceptInsecureCerts,
+        //     "sauce:options": {
+        //         ...defaultsMac
+        //     }
+        // },
+        // {
+        //     browserName: "MicrosoftEdge",
+        //     browserVersion: "latest",
+        //     platformName: "macOS 13",
+        //     acceptInsecureCerts,
+        //     "sauce:options": {
+        //         ...defaultsMac
+        //     }
+        // },
+        // {
+        //     browserName: "chrome",
+        //     browserVersion: "latest",
+        //     platformName: "macOS 13",
+        //     acceptInsecureCerts,
+        //     "sauce:options": {
+        //         ...defaultsMac
+        //     }
+        // },
+        // {
+        //     browserName: "chrome",
+        //     browserVersion: "latest",
+        //     platformName: "macOS 13",
+        //     acceptInsecureCerts,
+        //     "sauce:options": {
+        //         ...defaultsMac
+        //     }
+        // }
+    ]
+}
+
+export const config = Object.assign(shared, _config)
