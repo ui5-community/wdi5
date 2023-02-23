38c43acbe6febe10934ad1e37a6d30ca56c8783a
Volker Buzek <volker.buzek@js-soft.com>
2023-02-23 20:12:51 +0100

    refactor: move test file into flat hierarchy

--- examples/ui5-ts-app/test/e2e/protocol/wdio-ui5-webdriver.conf.ts
+++ examples/ui5-ts-app/test/e2e/protocol/wdio-ui5-webdriver.conf.ts
@@ -3,7 +3,7 @@ import { wdi5Config } from "wdio-ui5-service/dist/types/wdi5.types"
 export const config: wdi5Config = {
     baseUrl: "https://cswdev.launchpad.cfapps.eu10.hana.ondemand.com/site/csw#TestSampleTSapp-display",
     services: ["chromedriver", "ui5"],
-    specs: ["test/e2e/protocol/Protocol.test.ts"],
+    specs: ["test/e2e/Protocol.test.ts"],
     capabilities: [
         {
             browserName: "chrome"
