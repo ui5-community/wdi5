c5661e6b53bfaa3b58ba1c306c4fcea1ccdc0157
Volker Buzek <volker.buzek@js-soft.com>
2023-02-23 20:13:35 +0100

    test: use no auth url

--- examples/ui5-ts-app/test/e2e/protocol/wdio-ui5-devtools.conf.ts
+++ examples/ui5-ts-app/test/e2e/protocol/wdio-ui5-devtools.conf.ts
@@ -1,7 +1,7 @@
 import { wdi5Config } from "wdio-ui5-service/dist/types/wdi5.types"
 
 export const config: wdi5Config = {
-    baseUrl: "https://cswdev.launchpad.cfapps.eu10.hana.ondemand.com/site/csw#TestSampleTSapp-display",
+    baseUrl: "https://wdi5-sample-app.cfapps.eu20.hana.ondemand.com/no-auth/",
     services: ["ui5"] /* no drivers, so wdio is falling back to devtools*/,
     specs: ["test/e2e/Protocol.test.ts"],
     capabilities: [
