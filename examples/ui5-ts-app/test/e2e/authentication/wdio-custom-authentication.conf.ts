7e4b6bb498b0ea2818deb96b1865a0f9122f91c1
Volker Buzek <volker.buzek@js-soft.com>
2023-05-08 22:11:54 +0200

    test(custom): option to outsource to browserstack

--- examples/ui5-ts-app/test/e2e/authentication/wdio-custom-authentication.conf.ts
+++ examples/ui5-ts-app/test/e2e/authentication/wdio-custom-authentication.conf.ts
@@ -1,39 +1,53 @@
-import { config as baseConf } from "./wdio-base.conf"
-import { wdi5Capabilites } from "wdio-ui5-service/dist/types/wdi5.types"
 import merge from "ts-deepmerge"
+import { config as baseConf } from "./wdio-base.conf"
 
 // hack-y in these public (!) credentials
 process.env.wdi5_username = "tomsmith"
 process.env.wdi5_password = "SuperSecretPassword!"
 
-const capability: wdi5Capabilites = {
-    "wdi5:authentication": {
-        provider: "custom",
-        usernameSelector: "input[id='username']",
-        passwordSelector: "input[id='password']",
-        submitSelector: "button[type='submit']"
-    },
-    maxInstances: 5,
-    browserName: "chrome",
-    "goog:chromeOptions": {
-        args:
-            process.argv.indexOf("--headless") > -1
-                ? ["headless", "disable-gpu"]
-                : process.argv.indexOf("--debug") > -1
-                ? ["window-size=1440,800", "--auto-open-devtools-for-tabs"]
-                : ["window-size=1440,800"]
-    },
-    acceptInsecureCerts: true
+const authBlock = {
+    provider: "custom",
+    usernameSelector: "input[id='username']",
+    passwordSelector: "input[id='password']",
+    submitSelector: "button[type='submit']"
 }
+
 const _config = {
-    // we silently acknowledge that this is not a UI5 app
-    // being tested, so browser-scoped errors are ok
     baseUrl: "https://the-internet.herokuapp.com/login",
-    capabilities: [capability],
+    capabilities: baseConf.capabilities ? [...baseConf.capabilities] : [],
     wdi5: {
         skipInjectUI5OnStart: true
     }
 }
+
+delete baseConf.capabilities
+
+if (process.env.BROWSERSTACK) {
+    _config.capabilities = _config.capabilities.map((capability) => {
+        const enhancedCapability = {
+            ...capability,
+            "wdi5:authentication": authBlock
+        }
+        return enhancedCapability
+    })
+} else {
+    _config.capabilities = [
+        {
+            "wdi5:authentication": authBlock,
+            browserName: "chrome",
+            "goog:chromeOptions": {
+                args:
+                    process.argv.indexOf("--headless") > -1
+                        ? ["headless", "disable-gpu"]
+                        : process.argv.indexOf("--debug") > -1
+                        ? ["window-size=1440,800", "--auto-open-devtools-for-tabs"]
+                        : ["window-size=1440,800"]
+            },
+            acceptInsecureCerts: true
+        }
+    ]
+}
+
 const config = merge(baseConf, _config)
 config.specs = ["./test/e2e/Custom.test.ts"]
 export { config }
