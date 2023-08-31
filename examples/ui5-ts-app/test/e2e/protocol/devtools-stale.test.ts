3f6836b312173ae8d81b8500277dc9b6768921ac
Siolto <simon.coen@js-soft.com>
2023-08-31 13:14:35 +0200

    test: refactor stale tests and invalidate cache

--- examples/ui5-ts-app/test/e2e/protocol/devtools-stale.test.ts
+++ examples/ui5-ts-app/test/e2e/protocol/devtools-stale.test.ts
@@ -2,6 +2,10 @@ import { wdi5Selector } from "wdio-ui5-service/dist/types/wdi5.types"
 import Button from "sap/ui/webc/main/Button"
 
 describe("Devtools: ", async () => {
+    beforeEach(() => {
+        // clear cached wdi5Controls, so we can mock in each test seperately
+        browser._controls = []
+    })
     it("safeguard 'stale' element handling", async () => {
         const buttonWDI5 = await getButtonOnPage1()
 
@@ -17,19 +21,16 @@ describe("Devtools: ", async () => {
 
     it("safeguard 'stale' invisible element", async () => {
         const buttonWDI5 = await getButtonOnPage1()
+        expect(await buttonWDI5.isInitialized()).toBe(true)
 
-        // mock a stale element
-        buttonWDI5.getVisible = async function () {
-            return await this._executeControlMethod("getVisible", {
-                "element-6066-11e4-a52e-4f735466cecf": "stale"
-            })
-        }
-        // set stale element invisible
+        // make the element stale by manipulation DOM
         await buttonWDI5.setVisible(false)
 
         const invisibleButton = await getButtonOnPage1()
         // we should receive nothing => null
         expect(await invisibleButton.getVisible()).toBe(null)
+        // wdi5 control should be there but ui5 control is not initialized
+        expect(await invisibleButton.isInitialized()).toBe(false)
     })
 
     it("safeguard 'stale' element handling with full selector", async () => {
