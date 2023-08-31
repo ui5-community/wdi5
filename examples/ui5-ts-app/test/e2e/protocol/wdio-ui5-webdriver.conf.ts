673060f3de847fd70d3d6fc86e770d2cd6a0827d
Siolto <simon.coen@js-soft.com>
2023-08-31 10:22:48 +0200

    fix: webdriver tests

--- examples/ui5-ts-app/test/e2e/protocol/wdio-ui5-webdriver.conf.ts
+++ examples/ui5-ts-app/test/e2e/protocol/wdio-ui5-webdriver.conf.ts
@@ -1,10 +1,11 @@
 import { wdi5Config } from "wdio-ui5-service"
+import { resolve } from "path"
 
 export const config: wdi5Config = {
     baseUrl: "https://wdi5-sample-app.cfapps.eu20.hana.ondemand.com/no-auth/",
     services: ["ui5"],
-    specs: ["test/e2e/protocol/*.test.ts"],
-    exclude: ["test/e2e/protocol/devtools-stale.test.ts"],
+    specs: [resolve("test/e2e/protocol/*.test.ts")],
+    exclude: [resolve("test/e2e/protocol/devtools-stale.test.ts")],
     capabilities: [
         {
             browserName: "chrome",
