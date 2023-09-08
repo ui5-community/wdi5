e12c954d6457ccc71a9c20e9ac3f15399a4569df
Siolto <simon.coen@js-soft.com>
2023-09-08 11:49:06 +0200

    feat: refactor basic authenticator

--- src/lib/authentication/BasicAuthenticator.ts
+++ src/lib/authentication/BasicAuthenticator.ts
@@ -11,24 +11,21 @@ class BasicAuthenticator extends Authenticator {
         this.baseUrl = baseUrl
     }
     async login() {
-        if (!this.options.basicAuthUrls || this.options.basicAuthUrls.length === 0) {
-            // Authentication with baseUrl
-            const matches = await this.baseUrl.match(/(\w*:?\/\/)(.+)/)
-            const basicAuthUrls = matches[1] + this.getUsername() + ":" + this.getPassword() + "@" + matches[2]
-            await this.browserInstance.url(basicAuthUrls)
-        } else {
-            // Authentication with basicAuthUrls from BasicAuth
-            for (const basicAuthUrlsConfig of this.options.basicAuthUrls) {
-                const matches = basicAuthUrlsConfig.match(/(\w*:?\/\/)(.+)/)
-                const basicAuthUrls = matches[1] + this.getUsername() + ":" + this.getPassword() + "@" + matches[2]
-                await this.browserInstance.url(basicAuthUrls)
-            }
-        }
+        const basicAuthUrls = this.options.basicAuthUrls || [this.baseUrl]
+        await this.basicAuthLogin(basicAuthUrls)
         this.setIsLoggedIn(true)
         // trick 17 ;)
         // the preload flag of the OData models tries to load the $metadata to early for our authenticators.
         // We have to reload the application to "force" the application to reload the requests after we are authenticated.
         await this.browserInstance.url(this.baseUrl)
+    }
+
+    private async basicAuthLogin(basicAuthUrls: string[]) {
+        for (const basicAuthUrlsConfig of basicAuthUrls) {
+            const matches = basicAuthUrlsConfig.match(/(\w*:?\/\/)(.+)/)
+            const basicAuthUrls = matches[1] + this.getUsername() + ":" + this.getPassword() + "@" + matches[2]
+            await this.browserInstance.url(basicAuthUrls)
+        }
     }
 }
 
