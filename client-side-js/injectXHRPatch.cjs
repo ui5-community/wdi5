ed1c8f16380d5b62dc885977c21e7ff25c445e9f
Sebastian Mahr <sebastian.mahr@js-soft.com>
2024-01-11 10:32:09 +0100

    chore: typo

--- client-side-js/injectXHRPatch.cjs
+++ client-side-js/injectXHRPatch.cjs
@@ -16,16 +16,16 @@ async function clientSide_injectXHRPatch(config, browserInstance) {
             // Hook into XHR open for sinon XHRs
             const fnOriginalFakeOpen = sinon.FakeXMLHttpRequest.prototype.open
             sinon.FakeXMLHttpRequest.prototype.open = function () {
-                return fnOriginalFakeOpen.apply(this, hooketoXHROpen.apply(this, arguments))
+                return fnOriginalFakeOpen.apply(this, hooktoXHROpen.apply(this, arguments))
             }
 
             // Hook into XHR open for regular XHRs
             const fnOriginalOpen = XMLHttpRequest.prototype.open
             XMLHttpRequest.prototype.open = function () {
-                return fnOriginalOpen.apply(this, hooketoXHROpen.apply(this, arguments))
+                return fnOriginalOpen.apply(this, hooktoXHROpen.apply(this, arguments))
             }
 
-            function hooketoXHROpen(method, url, async) {
+            function hooktoXHROpen(method, url, async) {
                 //The ignore property will force the OPA5 _XHRWaiter to ignore certain calls for auto waiting
                 //https://github.com/SAP/openui5/blob/45e49887f632d0a8a8ef195bd3edf10eb0be9015/src/sap.ui.core/src/sap/ui/test/autowaiter/_XHRWaiter.js
                 //This ist the XHR request instance so setting it here will only affect the specific request
