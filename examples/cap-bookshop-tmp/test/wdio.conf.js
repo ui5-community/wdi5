1016f79c533da57c5f683623fb6b50104757ac18
Volker Buzek <volker.buzek@js-soft.com>
2024-05-28 11:53:49 +0200

    test: wdio conf

--- examples/cap-bookshop-tmp/test/wdio.conf.js
+++ examples/cap-bookshop-tmp/test/wdio.conf.js
@@ -142,7 +142,7 @@ exports.config = {
     // If you are using Cucumber you need to specify the location of your step definitions.
     cucumberOpts: {
         // <string[]> (file/dir) require files before executing features
-        require: ["./features/**/step-definitions/steps.js"],
+        require: ["./test/lib-cds-cucumber/steps/**/*.js"],
         // <boolean> show full backtrace for errors
         backtrace: false,
         // <string[]> ("extension:module") require files with the given EXTENSION after requiring MODULE (repeatable)
