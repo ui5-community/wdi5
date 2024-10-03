ed33d3fbe5c0ca680f2b7cae0c1e628ae9d5bf7d
Siolto <simon.coen@js-soft.com>
2024-10-03 13:49:07 +0200

    refactor: rewrite _getAggregation

--- client-side-js/_getAggregation.cjs
+++ client-side-js/_getAggregation.cjs
@@ -1,25 +1,26 @@
 async function clientSide_getAggregation(webElement, aggregationName, browserInstance) {
     webElement = await Promise.resolve(webElement) // to plug into fluent async api
     browserInstance = await Promise.resolve(browserInstance)
-    return await browserInstance.executeAsync(
-        (webElement, aggregationName, done) => {
-            window.bridge.waitForUI5(window.wdi5.waitForUI5Options).then(() => {
-                // DOM to UI5
-                try {
-                    let oControl = window.wdi5.getUI5CtlForWebObj(webElement)
-                    let cAggregation = oControl.getAggregation(aggregationName)
-                    // if getAggregation retrieves an element only it has to be transformed to an array
-                    if (cAggregation && !Array.isArray(cAggregation)) {
-                        cAggregation = [cAggregation]
-                    }
-                    // read classname eg. sap.m.ComboBox
-                    controlType = oControl.getMetadata()._sClassName
-                    let result = window.wdi5.createControlIdMap(cAggregation, controlType)
-                    done({ status: 0, result: result })
-                } catch (e) {
-                    done({ status: 1, message: e.toString() })
-                }
+    return await browserInstance.execute(
+        async (webElement, aggregationName) => {
+            await window.bridge.waitForUI5(window.wdi5.waitForUI5Options).catch((e) => {
+                return { status: 1, message: e.toString() }
             })
+
+            try {
+                let oControl = window.wdi5.getUI5CtlForWebObj(webElement)
+                let cAggregation = oControl.getAggregation(aggregationName)
+                // if getAggregation retrieves an element only it has to be transformed to an array
+                if (cAggregation && !Array.isArray(cAggregation)) {
+                    cAggregation = [cAggregation]
+                }
+                // read classname eg. sap.m.ComboBox
+                controlType = oControl.getMetadata()._sClassName
+                let result = window.wdi5.createControlIdMap(cAggregation, controlType)
+                return { status: 0, result: result }
+            } catch (e) {
+                return { status: 1, message: e.toString() }
+            }
         },
         webElement,
         aggregationName
