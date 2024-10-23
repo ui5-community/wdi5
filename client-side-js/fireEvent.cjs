bdf7ce485be1e2f46f6065757692d04597c89e14
Volker Buzek <volker.buzek@js-soft.com>
2024-10-23 16:44:41 +0200

    wip: make .press and .fireEvent work

--- client-side-js/fireEvent.cjs
+++ client-side-js/fireEvent.cjs
@@ -1,32 +1,30 @@
+//> REVISIT: do we need this at all?
+// -> as .fireEvent on a UI5 control in Node.js-scope is discouraged
 async function clientSide_fireEvent(webElement, eventName, oOptions, browserInstance) {
-    return await browserInstance.executeAsync(
-        (webElement, eventName, oOptions, done) => {
-            window.wdi5.waitForUI5(
-                window.wdi5.waitForUI5Options,
-                () => {
-                    window.wdi5.Log.info("[browser wdi5] working " + eventName + " for " + webElement)
-                    // DOM to ui5
-                    let oControl = window.wdi5.getUI5CtlForWebObj(webElement)
-                    if (oControl && oControl.hasListeners(eventName)) {
-                        window.wdi5.Log.info("[browser wdi5] firing " + eventName + " on " + webElement)
-                        // element existent and has the target event
-                        try {
-                            // eval the options indicated by option of type string
-                            if (typeof oOptions === "string") {
-                                oOptions = eval(oOptions)()
-                            }
-                            oControl.fireEvent(eventName, oOptions)
-                            // convert to boolean
-                            done({ status: 0, result: true })
-                        } catch (e) {
-                            window.wdi5.errorHandling.bind(this, done)
-                        }
-                    } else {
-                        window.wdi5.errorHandling(this, done)
+    return await browserInstance.execute(
+        async (webElement, eventName, oOptions) => {
+            await window.wdi5.waitForUI5(window.wdi5.waitForUI5Options).catch(window.wdi5.errorHandling.bind(this))
+
+            window.wdi5.Log.info("[browser wdi5] working " + eventName + " for " + webElement)
+            // DOM to ui5
+            let oControl = window.wdi5.getUI5CtlForWebObj(webElement)
+            if (oControl && oControl.hasListeners(eventName)) {
+                window.wdi5.Log.info("[browser wdi5] firing " + eventName + " on " + webElement)
+                // element existent and has the target event
+                try {
+                    // eval the options indicated by option of type string
+                    if (typeof oOptions === "string") {
+                        oOptions = eval(oOptions)()
                     }
-                },
-                window.wdi5.errorHandling.bind(this, done)
-            )
+                    oControl.fireEvent(eventName, oOptions)
+                    // convert to boolean
+                    return { status: 0, result: true }
+                } catch (error) {
+                    window.wdi5.errorHandling.bind(this, error)
+                }
+            } else {
+                return window.wdi5.errorHandling("no control and/or no event listener found for " + eventName)
+            }
         },
         webElement,
         eventName,
