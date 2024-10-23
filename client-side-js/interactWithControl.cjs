9426149c323e287312c82a432d00b9d815c57a34
Volker Buzek <volker.buzek@js-soft.com>
2024-10-23 17:13:58 +0200

    wip: make it work
    Co-authored-by: Simon Coen <Siolto@users.noreply.github.com>

--- client-side-js/interactWithControl.cjs
+++ client-side-js/interactWithControl.cjs
@@ -1,21 +1,17 @@
 async function clientSide_interactWithControl(oOptions, browserInstance) {
     browserInstance = await Promise.resolve(browserInstance)
-    return await browserInstance.executeAsync((oOptions, done) => {
-        window.wdi5.waitForUI5(
-            window.wdi5.waitForUI5Options,
-            () => {
-                window.wdi5.Log.info("[browser wdi5] locating controlSelector")
-                oOptions.selector = window.wdi5.createMatcher(oOptions.selector)
-                window.bridge
-                    .interactWithControl(oOptions)
-                    .then((result) => {
-                        window.wdi5.Log.info("[browser wdi5] interaction complete! - Message: " + result)
-                        done({ status: 0, result: result })
-                    })
-                    .catch(window.wdi5.errorHandling.bind(this, done))
-            },
-            window.wdi5.errorHandling.bind(this, done)
-        )
+    return await browserInstance.execute(async (oOptions) => {
+        await window.wdi5.waitForUI5(window.wdi5.waitForUI5Options).catch(window.wdi5.errorHandling.bind(this))
+        window.wdi5.Log.info("[browser wdi5] locating controlSelector")
+        oOptions.selector = window.wdi5.createMatcher(oOptions.selector)
+        try {
+            const result = await window.bridge.interactWithControl(oOptions)
+            window.wdi5.Log.info("[browser wdi5] interaction complete! - Message: " + result)
+            return { status: 0, result }
+        } catch (err) {
+            // this return "status" : 1 as part of the returned object
+            return window.wdi5.errorHandling(err)
+        }
     }, oOptions)
 }
 
