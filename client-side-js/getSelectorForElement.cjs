d3dbfbdf4726066837a973d2c3bdcdd03d4cfa2c
Volker Buzek <volker.buzek@js-soft.com>
2024-10-23 16:51:10 +0200

    wip: make work, but missing test

--- client-side-js/getSelectorForElement.cjs
+++ client-side-js/getSelectorForElement.cjs
@@ -1,20 +1,15 @@
+// TODO: Test is missing!
 async function clientSide_getSelectorForElement(oOptions, browserInstance) {
-    return await browserInstance.executeAsync((oOptions, done) => {
-        window.wdi5.waitForUI5(
-            window.wdi5.waitForUI5Options,
-            () => {
-                window.wdi5.Log.info("[browser wdi5] locating domElement")
-                window.bridge
-                    .findControlSelectorByDOMElement(oOptions)
-                    .then((controlSelector) => {
-                        window.wdi5.Log.info("[browser wdi5] controlLocator created!")
-                        done({ status: 0, result: controlSelector })
-                        return controlSelector
-                    })
-                    .catch(window.wdi5.errorHandling.bind(this, done))
-            },
-            window.wdi5.errorHandling.bind(this, done)
-        )
+    return await browserInstance.execute(async (oOptions) => {
+        await window.wdi5.waitForUI5(window.wdi5.waitForUI5Options).catch(window.wdi5.errorHandling.bind(this))
+
+        window.wdi5.Log.info("[browser wdi5] locating domElement")
+        const controlSelector = await window.bridge
+            .findControlSelectorByDOMElement(oOptions)
+            .catch(window.wdi5.errorHandling.bind(this))
+
+        window.wdi5.Log.info("[browser wdi5] controlLocator created!")
+        return { status: 0, result: controlSelector }
     }, oOptions)
 }
 
