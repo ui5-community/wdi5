c8c2955c87509a8f96bdff983267d76c851d5ab7
Siolto <simon.coen@js-soft.com>
2025-02-12 18:54:24 +0100

    fix: improve error handling and result processing in executeControlMethod

--- client-side-js/executeControlMethod.cjs
+++ client-side-js/executeControlMethod.cjs
@@ -13,111 +13,114 @@ if (global.browser) {
 async function executeControlMethod(webElement, methodName, browserInstance, args) {
     return await browserInstance.execute(
         async (webElement, methodName, args) => {
-            await window.bridge.waitForUI5(window.wdi5.waitForUI5Options)
-            // window.wdi5.errorHandling.bind(this, null, reject)
+            try {
+                await window.bridge.waitForUI5(window.wdi5.waitForUI5Options)
 
-            // DOM to UI5
-            const oControl = window.wdi5.getUI5CtlForWebObj(webElement)
+                // DOM to UI5
+                const oControl = window.wdi5.getUI5CtlForWebObj(webElement)
 
-            // execute the function
-            let result = oControl[methodName].apply(oControl, args)
-            const metadata = oControl.getMetadata()
+                // execute the function
+                let result = oControl[methodName].apply(oControl, args)
+                const metadata = oControl.getMetadata()
 
-            if (Array.isArray(result)) {
-                if (result.length === 0) {
-                    return { status: 0, result: result, returnType: "empty" }
-                } else if (result[0]?.getParent) {
-                    // expect the method call delivers non-primitive results (like getId())
-                    // but delivers a complex/structured type
-                    // -> currenlty, only getAggregation(...) is supported
-                    // read classname eg. sap.m.ComboBox
-                    controlType = oControl.getMetadata()._sClassName
+                if (Array.isArray(result)) {
+                    if (result.length === 0) {
+                        return { status: 0, result: result, returnType: "empty" }
+                    } else if (result[0]?.getParent) {
+                        // expect the method call delivers non-primitive results (like getId())
+                        // but delivers a complex/structured type
+                        // -> currenlty, only getAggregation(...) is supported
+                        // read classname eg. sap.m.ComboBox
+                        controlType = oControl.getMetadata()._sClassName
 
-                    result = window.wdi5.createControlIdMap(result, controlType)
-                    return { status: 0, result: result, returnType: "aggregation" }
-                } else {
-                    return { status: 0, result: result, returnType: "result" }
-                }
-            } else {
-                // ui5 api <control>.focus() doesn't have return value
-                if (methodName === "focus" && result === undefined) {
-                    return {
-                        status: 0,
-                        result: `called focus() on wdi5 representation of a ${metadata.getElementName()}`,
-                        returnType: "element"
-                    }
-                } else if (methodName === "exec" && result && result.status > 0) {
-                    return {
-                        status: result.status,
-                        message: result.message
-                    }
-                } else if (result === undefined || result === null) {
-                    return {
-                        status: 1,
-                        result: `function ${methodName} does not exist on control ${metadata.getElementName()}!`,
-                        returnType: "none"
-                    }
-                } else {
-                    if (window.wdi5.isPrimitive(result)) {
+                        result = window.wdi5.createControlIdMap(result, controlType)
+                        return { status: 0, result: result, returnType: "aggregation" }
+                    } else {
                         return { status: 0, result: result, returnType: "result" }
-                    } else if (
-                        // we have an object that is not a UI5 control
-                        typeof result === "object" &&
-                        result !== null &&
-                        !(result instanceof sap.ui.core.Control) &&
-                        !(result instanceof sap.ui.core.Item)
-                    ) {
-                        // save before manipulate
-                        const uuid = window.wdi5.saveObject(result)
-
-                        // FIXME: extract, collapse and remove cylic in 1 step
-                        // extract the methods first
-                        const aProtoFunctions = window.wdi5.retrieveControlMethods(result, true)
-
-                        // flatten the prototype so we have all funcs available
-                        const collapsed = window.wdi5.collapseObject(result)
-                        // exclude cyclic references
-                        const collapsedAndNonCyclic = JSON.parse(
-                            JSON.stringify(collapsed, window.wdi5.getCircularReplacer())
-                        )
-                        // remove all empty Array elements, inlcuding private keys (starting with "_")
-                        const semanticCleanedElements = window.wdi5.removeEmptyElements(collapsedAndNonCyclic)
-
+                    }
+                } else {
+                    // ui5 api <control>.focus() doesn't have return value
+                    if (methodName === "focus" && result === undefined) {
                         return {
                             status: 0,
-                            object: semanticCleanedElements,
-                            returnType: "object",
-                            aProtoFunctions: aProtoFunctions,
-                            uuid: uuid,
-                            nonCircularResultObject: semanticCleanedElements
+                            result: `called focus() on wdi5 representation of a ${metadata.getElementName()}`,
+                            returnType: "element"
                         }
-                    } else if (
-                        typeof result === "object" &&
-                        result !== null &&
-                        // wdi5 returns a wdi5 control if the UI5 api return its control
-                        // allows method chaining
-                        !(result instanceof sap.ui.base.Object)
-                    ) {
+                    } else if (methodName === "exec" && result && result.status > 0) {
                         return {
-                            status: 2,
-                            returnType: "unknown"
+                            status: result.status,
+                            message: result.message
+                        }
+                    } else if (result === undefined || result === null) {
+                        return {
+                            status: 1,
+                            result: `function ${methodName} does not exist on control ${metadata.getElementName()}!`,
+                            returnType: "none"
                         }
                     } else {
-                        // we got ourselves a regular UI5 control
-                        // check that we're not working against ourselves :)
-                        if (result && result.getId && oControl.getId() !== result.getId()) {
-                            // ui5 function like get parent might return another ui5 control -> return it to check with this wdi5 instance
-                            result = window.wdi5.createControlId(result)
-                            return { status: 0, result: result, returnType: "newElement" }
-                        } else {
+                        if (window.wdi5.isPrimitive(result)) {
+                            return { status: 0, result: result, returnType: "result" }
+                        } else if (
+                            // we have an object that is not a UI5 control
+                            typeof result === "object" &&
+                            result !== null &&
+                            !(result instanceof sap.ui.core.Control) &&
+                            !(result instanceof sap.ui.core.Item)
+                        ) {
+                            // save before manipulate
+                            const uuid = window.wdi5.saveObject(result)
+
+                            // FIXME: extract, collapse and remove cylic in 1 step
+                            // extract the methods first
+                            const aProtoFunctions = window.wdi5.retrieveControlMethods(result, true)
+
+                            // flatten the prototype so we have all funcs available
+                            const collapsed = window.wdi5.collapseObject(result)
+                            // exclude cyclic references
+                            const collapsedAndNonCyclic = JSON.parse(
+                                JSON.stringify(collapsed, window.wdi5.getCircularReplacer())
+                            )
+                            // remove all empty Array elements, inlcuding private keys (starting with "_")
+                            const semanticCleanedElements = window.wdi5.removeEmptyElements(collapsedAndNonCyclic)
+
                             return {
                                 status: 0,
-                                result: `instance of wdi5 representation of a ${metadata.getElementName()}`,
-                                returnType: "element"
+                                object: semanticCleanedElements,
+                                returnType: "object",
+                                aProtoFunctions: aProtoFunctions,
+                                uuid: uuid,
+                                nonCircularResultObject: semanticCleanedElements
+                            }
+                        } else if (
+                            typeof result === "object" &&
+                            result !== null &&
+                            // wdi5 returns a wdi5 control if the UI5 api return its control
+                            // allows method chaining
+                            !(result instanceof sap.ui.base.Object)
+                        ) {
+                            return {
+                                status: 2,
+                                returnType: "unknown"
+                            }
+                        } else {
+                            // we got ourselves a regular UI5 control
+                            // check that we're not working against ourselves :)
+                            if (result && result.getId && oControl.getId() !== result.getId()) {
+                                // ui5 function like get parent might return another ui5 control -> return it to check with this wdi5 instance
+                                result = window.wdi5.createControlId(result)
+                                return { status: 0, result: result, returnType: "newElement" }
+                            } else {
+                                return {
+                                    status: 0,
+                                    result: `instance of wdi5 representation of a ${metadata.getElementName()}`,
+                                    returnType: "element"
+                                }
                             }
                         }
                     }
                 }
+            } catch (error) {
+                window.wdi5.errorHandling.bind(error)
             }
         },
         webElement,
