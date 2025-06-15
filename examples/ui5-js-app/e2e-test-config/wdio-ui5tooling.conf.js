e14cf48862dfa4fbc1d380307b471d8c3159232d
Mauricio Lauffer <443888+mauriciolauffer@users.noreply.github.com>
2025-06-15 15:13:52 +1000

    test: fix wdio-ui5tooling.conf.js

--- examples/ui5-js-app/e2e-test-config/wdio-ui5tooling.conf.js
+++ examples/ui5-js-app/e2e-test-config/wdio-ui5tooling.conf.js
@@ -3,7 +3,11 @@ const { baseConfig } = require("./wdio.base.conf")
 const merge = require("deepmerge")
 
 const _config = {
-    specs: [join("..", "webapp", "test", "e2e", "basic.test.js"), join("webapp", "test", "e2e", "hash-nav.test.js")],
+    specs: [
+        join("..", "webapp", "test", "e2e", "basic.test.js"),
+        join("..", "webapp", "test", "e2e", "hash-nav.test.js")
+    ],
+    // TODO: this test only works when served from the ui5-tooling server ($ ui5 serve)
     baseUrl: "http://localhost:8080/index.html?isui5toolingTest=true",
     wdi5: {
         ignoreAutoWaitUrls: [".*/Categories.*"]
