52710e858e30241750f156e97a68cff6f53341be
I586943 <jimmy.xu03@sap.com>
2025-07-07 10:17:52 +0800

    fix: assign value to idpDomainOpt

--- src/lib/authentication/BTPAuthenticator.ts
+++ src/lib/authentication/BTPAuthenticator.ts
@@ -11,7 +11,7 @@ class BTPAuthenticator extends Authenticator {
         this.usernameSelector = options.usernameSelector ?? "#j_username"
         this.passwordSelector = options.passwordSelector ?? "#j_password"
         this.submitSelector = options.submitSelector ?? "#logOnFormSubmit"
-        this.idpDomainOpt = this.idpDomain
+        this.idpDomainOpt = options.idpDomain
         this.disableBiometricAuth = options.disableBiometricAuthentication ?? false
         if (this.disableBiometricAuth) {
             if (!options.idpDomain) {
