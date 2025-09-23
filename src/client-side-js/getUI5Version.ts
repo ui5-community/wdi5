16d6836c2fe9d030b5bc770a63bb188cfc11c7d6
Mauricio Lauffer <443888+mauriciolauffer@users.noreply.github.com>
2025-09-23 12:34:31 -0300

    refactor: reuse getUI5Version

--- src/client-side-js/getUI5Version.ts
+++ src/client-side-js/getUI5Version.ts
@@ -8,7 +8,7 @@ async function clientSide_getUI5Version(browserInstance: WebdriverIO.Browser) {
     return await browserInstance.execute(async () => {
         return await new Promise<string>((resolve) => {
             sap.ui.require(["sap/ui/VersionInfo"], async (VersionInfo: VersionInfo) => {
-                const versionInfo = (await VersionInfo.load()) as LibraryInfo
+                const versionInfo = (await VersionInfo.load({ library: "sap.ui.core" })) as LibraryInfo
                 resolve(versionInfo.version)
             })
         })
