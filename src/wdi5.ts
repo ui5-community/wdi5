46b0b3b205b491e39dd23414c1d3184fe94902e2
Volker Buzek <6573426+vobu@users.noreply.github.com>
2025-10-12 11:24:05 +0200

    chore(v3): final touches (#690)
    [skip ci]
    
    * fix(ts-app,deploy): auth url
    
    * refactor: simplify goTo method signatures and update documentation
    
    * fix: provide test:protocol npm script stub
    
    * fix: replace npm ci with npm i in workflow for package installation

--- src/wdi5.ts
+++ src/wdi5.ts
@@ -83,27 +83,23 @@ export class wdi5 {
     }
      * @param browserInstance the currently remote controlled browser
      */
-    static async goTo(routerOption: any, browserInstance?: WebdriverIO.Browser)
     /**
-     * navigate to a route/view of a UI5 app - by router object
+     * navigate to a route/view of a UI5 app
      *
-     * @param withSHash a UI5 router object, e.g. {sHash:"#/accounts/create"}
+     * @param target either a string hash (e.g. "#/accounts/create") or a router descriptor object (e.g. { sHash:"#/accounts/create"} or UI5 router object)
      * @param browserInstance the currently remote controlled browser
      */
-    static async goTo(withSHash: any, browserInstance?: WebdriverIO.Browser)
-    /**
-     * navigate to a route/view of a UI5 app - by string hash
-     *
-     * @hash hash hash-part of the URL, e.g. "#/accounts/create"
-     * @param browserInstance the currently remote controlled browser
-     */
-    static async goTo(hash: string, browserInstance?: WebdriverIO.Browser)
+    static async goTo(target: string | Record<string, any>, browserInstance?: WebdriverIO.Browser)
 
     /**
      * @deprecated please supply only a single parameter to .goTo() and optionally a browser instance
      */
     static async goTo(param: any, oRoute: any, browserInstance?: WebdriverIO.Browser)
-    static async goTo(byWhat, oRoute: any, browserInstance: WebdriverIO.Browser = browser) {
+    static async goTo(
+        byWhat: string | Record<string, any>,
+        oRoute?: any,
+        browserInstance: WebdriverIO.Browser = browser
+    ) {
         if (oRoute) {
             Logger.getInstance().warn(
                 "deprecated signature: please use single parameter as nav target: wdi5.goTo(target)"
