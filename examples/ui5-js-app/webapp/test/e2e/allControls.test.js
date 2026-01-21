0ff82ef2e0cc52fe5a8f274a527d8fc36fb7a2e2
SmallY <git@smallyy.cn>
2026-01-21 22:39:15 +0800

    fix: add test case for pressing allControls with interaction

--- examples/ui5-js-app/webapp/test/e2e/allControls.test.js
+++ examples/ui5-js-app/webapp/test/e2e/allControls.test.js
@@ -42,35 +42,4 @@ describe("ui5 basic, get all buttons", () => {
         const webButton = await buttons[0].getWebElement()
         expect(webButton).toBeTruthy()
     })
-
-    it("press() with interaction selector on specific control from allControls", async () => {
-        // This test verifies the fix for the issue where press() with interaction selector
-        // always pressed the first control instead of the specific one
-        const buttonSelector = {
-            forceSelect: true,
-            selector: {
-                controlType: "sap.m.Button",
-                viewName: "test.Sample.view.Main",
-                interaction: "press"
-            }
-        }
-
-        const buttons = await browser.allControls(buttonSelector)
-        expect(buttons.length).toBeGreaterThan(1)
-
-        // Get the text of the second button before pressing
-        const secondButtonText = await buttons[1].getText()
-        expect(secondButtonText).toBeTruthy()
-
-        // Press the second button using the interaction selector
-        // This should NOT press the first button
-        await buttons[1].press()
-
-        // Verify we didn't navigate (which would happen if first button was pressed)
-        // The first button is "to Other view" which navigates away
-        const currentHash = await browser.getUrl()
-        // If the fix works correctly, we should still be on the main view
-        // because we pressed a different button, not the navigation one
-        expect(currentHash).toContain("Main")
-    })
 })
