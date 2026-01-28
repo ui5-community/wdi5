3d53e735bd81102618dfe8e2340c74bb2e872ce2
Gregor Wolf <gregor.wolf@gmail.com>
2026-01-28 08:46:05 +0100

    docs: link to the test library documentation (#729)
    * docs: link to the test library documentation
    
    * fix: replace sapui5.hana.ondemand.com with ui5.sap.com

--- examples/cucumber/wdio.conf.js
+++ examples/cucumber/wdio.conf.js
@@ -112,7 +112,7 @@ exports.config = {
     // with `/`, the base url gets prepended, not including the path portion of your baseUrl.
     // If your `url` parameter starts without a scheme or `/` (like `some/path`), the base url
     // gets prepended directly.
-    baseUrl: "https://sapui5.hana.ondemand.com",
+    baseUrl: "https://ui5.sap.com",
     //
     // Default timeout for all waitFor* commands.
     waitforTimeout: 10000,
